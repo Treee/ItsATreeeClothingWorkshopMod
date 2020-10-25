3ca6209a15f62c669273ef92ccb93242dce78d18
Treee <jonathon.lindemann@gmail.com>
2020-10-25 15:25:14 -0600

    adding meta data for mod

--- @ItsATreeeClothing/Addons/FBI_Clothing/config.cpp
+++ @ItsATreeeClothing/Addons/FBI_Clothing/config.cpp
@@ -1,40 +1,53 @@
 class CfgPatches
 {
-  class DZ_Characters_Tops
-  {
-    units[]=
-    {
-      "Shirt_CheckRed",
-      "Hoodie_Blue",
-      "Hoodie_Black",
-      "Hoodie_Brown",
-      "Hoodie_Green",
-      "Hoodie_Grey",
-      "Hoodie_Red",
-      "MaleTorso",
-      "FemaleTorso"
-    };
-    weapons[]={};
-    requiredVersion=0.1;
-    requiredAddons[]=
-    {
-        "DZ_Characters"
-    };
+  class FBI_Clothing {
+    units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] =
+		{
+			"DZ_Characters_Tops",
+      "DZ_Characters_Vests"
+		};
+		author = "ItsATreee";
+		name = "Treee";
+		url = "https://github.com/Treee/ItsATreeeDayZWorkshopMods/tree/master/ItsATreeeClothingSource";
   };
-  class DZ_Characters_Vests
-	{
-		units[]=
-		{
-			"TacticalVest",
-			"UKAssVest"
-		};
-		weapons[]={};
-		requiredVersion=0.1;
-		requiredAddons[]=
-		{
-			"DZ_Characters"
-		};
-	};
+  // class DZ_Characters_Tops
+  // {
+  //   units[]=
+  //   {
+  //     "Shirt_CheckRed",
+  //     "Hoodie_Blue",
+  //     "Hoodie_Black",
+  //     "Hoodie_Brown",
+  //     "Hoodie_Green",
+  //     "Hoodie_Grey",
+  //     "Hoodie_Red",
+  //     "MaleTorso",
+  //     "FemaleTorso"
+  //   };
+  //   weapons[]={};
+  //   requiredVersion=0.1;
+  //   requiredAddons[]=
+  //   {
+  //       "DZ_Characters"
+  //   };
+  // };
+  // class DZ_Characters_Vests
+	// {
+	// 	units[]=
+	// 	{
+	// 		"TacticalVest",
+	// 		"UKAssVest"
+	// 	};
+	// 	weapons[]={};
+	// 	requiredVersion=0.1;
+	// 	requiredAddons[]=
+	// 	{
+	// 		"DZ_Characters"
+	// 	};
+	// };
 };
 
 class CfgVehicles 
@@ -48,21 +61,26 @@ class CfgVehicles
   // class PlateCarrierVest;
   class HighCapacityVest_ColorBase;
 
+  // https://github.com/BohemiaInteractive/DayZ-Samples/blob/master/Test_ClothingRetexture/config.cpp
+
   class ItsaTreee_FBI_BomberJacket_Blue: BomberJacket_ColorBase
   {
     scope=2;
+    displayName = "FBI Bomber Jacket";
+    descriptionShort = "A jacket with the letters FBI embroidered on the front and back.";
     hiddenSelectionsTextures[]=
     {
-      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa",
-      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa",
-      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa"
+      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa",     // on-ground texture
+      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa",     // on-character texture
+      "FBI_Clothing\data\bomberjacket_blue_co_fbi.paa"      // on-character texture
     };
   };
 
   class ItsaTreee_FBI_JumpsuitJacket_Blue: JumpsuitJacket_ColorBase
   {
     scope=2;
-    visibilityModifier=0.80000001;
+    displayName = "FBI Jumpsuit Jacket";
+    descriptionShort = "A jacket with the letters FBI embroidered on the front and back.";
     hiddenSelectionsTextures[]=
     {
       "FBI_Clothing\data\jumpsuit_top_blue_co_fbi.paa",
@@ -74,7 +92,8 @@ class CfgVehicles
   class ItsaTreee_FBI_M65Jacket_Black: M65Jacket_ColorBase
   {
     scope=2;
-    visibilityModifier=0.69999999;
+    displayName = "FBI M65 Jacket";
+    descriptionShort = "A jacket with the letters FBI embroidered on the front and back.";
     hiddenSelectionsTextures[]=
     {
       "FBI_Clothing\data\m65_jacket_black_co_fbi.paa",
@@ -86,7 +105,8 @@ class CfgVehicles
   class ItsaTreee_FBI_PressVest_Blue: PressVest_ColorBase
   {
     scope=2;
-    visibilityModifier=0.94999999;
+    displayName = "FBI Press Vest";
+    descriptionShort = "A vest with the letters FBI embroidered on the front and back.";
     hiddenSelectionsTextures[]=
     {
       "FBI_Clothing\data\pressvest_blue_co_fbi.paa",
@@ -98,6 +118,8 @@ class CfgVehicles
   class ItsaTreee_FBI_PoliceVest: PoliceVest
   {
     scope=2;
+    displayName = "FBI Police Vest";
+    descriptionShort = "A vest with the letters FBI embroidered on the front and back.";
     hiddenSelectionsTextures[]=
     {
       "FBI_Clothing\data\policevest_co_fbi.paa",
@@ -121,7 +143,8 @@ class CfgVehicles
   class ItsaTreee_FBI_HighCapacityVest_Black: HighCapacityVest_ColorBase
 	{
 		scope=2;
-		visibilityModifier=0.69999999;
+    displayName = "FBI High Capacity Vest";
+    descriptionShort = "A vest with the letters FBI embroidered on the front and back.";
 		hiddenSelectionsTextures[]=
 		{
 			"FBI_Clothing\data\tacticalvest2_black_co_fbi.paa",
@@ -133,7 +156,8 @@ class CfgVehicles
   class ItsaTreee_FBI_HighCapacityVest_Olive: HighCapacityVest_ColorBase
 	{
 		scope=2;
-		visibilityModifier=0.60000002;
+    displayName = "FBI High Capacity Vest";
+    descriptionShort = "A vest with the letters FBI embroidered on the front and back.";
 		hiddenSelectionsTextures[]=
 		{
 			"FBI_Clothing\data\tacticalvest2_olive_co_fbi.paa",
