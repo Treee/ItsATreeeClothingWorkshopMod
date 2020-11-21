bd34cf9f169eedc0b52482584a4d0adc9f408ae8
Treee <jonathon.lindemann@gmail.com>
2020-11-21 14:01:10 -0700

    face paint test

--- @ItsATreeeSurvivalistsRetextures/Addons/Survivalists_Characters/config.cpp
+++ @ItsATreeeSurvivalistsRetextures/Addons/Survivalists_Characters/config.cpp
@@ -6632,4 +6632,55 @@ class CfgVehicles
 
 
 
+
+  class Head;
+  class SurvivorFemale_Base;
+
+  class IAT_C_FemaleEvaHead: Head_Default
+	{
+		class Default
+		{
+			displayname="$STR_CFG_FACES_Default";
+			texture="Survivalists_Characters\data\camo_facepaint_hhl_f_eva_body_co.paa";
+			head="IAT_C_FemaleEvaHead";
+			material="Survivalists_Characters\data\camo_hhl_f_eva_body.rvmat";
+		};
+  };
+
+  class IAT_C_SurvivorF_Eva: SurvivorFemale_Base
+	{
+		scope=2;
+		emptyHead="IAT_C_FemaleEvaHead";
+		emptyBody="FemaleEvaTorso";
+		emptyLegs="FemaleEvaLegs";
+		emptyFeet="FemaleEvaFeet";
+		emptyGloves="FemaleEvaHands";
+		faceType="IAT_C_FemaleEvaHead";
+		decayedTexture="\DZ\characters\heads\data\f_Eva_2\hhl_f_Eva_body_decayed_co.paa";
+		class BloodyHands
+		{
+			mat_normal="Survivalists_Characters\data\camo_hhl_f_eva_body.rvmat";
+			mat_blood="dz\characters\heads\data\f_eva_2\hhl_f_eva_body_bloodyhands.rvmat";
+		};
+		class Lifespan
+		{
+			class Beard
+			{
+				mat[]={};
+			};
+		};
+		class Wounds
+		{
+			tex[]={};
+			mat[]=
+			{
+				"Survivalists_Characters\data\camo_hhl_f_eva_body.rvmat",
+				"dz\characters\heads\data\f_eva_2\hhl_f_eva_body_injury.rvmat",
+				"dz\characters\heads\data\f_eva_2\hhl_f_eva_body_injury2.rvmat"
+			};
+		};
+		voiceType=1;
+	};
+	
+
 };
