b13b132baecb23bf4d050b5b2f5fbb5ecd734465
Treee <jonathon.lindemann@gmail.com>
2020-11-21 14:11:20 -0700

    pulling face paint into its own mod directory. adding initial commit of mute transmitters

---
+++ @ItsATreeeMuteTransmitter/scripts/5_mission/ItemActionsWidgetRP.c
@@ -0,0 +1,45 @@
+modded class ItemActionsWidget extends ScriptedWidgetEventHandler
+{
+	
+	override protected void SetItemDesc(EntityAI entity, string descText, string itemWidget, string descWidget)
+	{
+		Widget widget;
+		
+		widget = m_Root.FindAnyWidget(itemWidget);
+		TextWidget itemName;
+		if(entity)
+		{
+			if (entity.IsTransmitter())
+			{
+				TransmitterBase radio = TransmitterBase.Cast(entity);
+				descText.ToUpper();
+				
+				Class.CastTo(itemName, widget.FindAnyWidget(descWidget));
+				if(radio)
+				{
+					if(radio.IsMuted())
+					{
+						itemName.SetText(descText + " WYCISZONE");
+					} else
+					{
+						itemName.SetText(descText);
+					}
+				}
+
+				widget.Show(true);
+			} else 
+			{
+				descText.ToUpper();
+				
+				Class.CastTo(itemName, widget.FindAnyWidget(descWidget));
+				itemName.SetText(descText);
+				widget.Show(true);
+			}
+			
+
+		}
+		else
+			widget.Show(false);
+	}
+	
+}
