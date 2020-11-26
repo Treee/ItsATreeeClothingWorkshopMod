16c7ca939f935cb38b3e83a39b2494865cd47fb5
Treee <jonathon.lindemann@gmail.com>
2020-11-26 11:45:38 -0700

    adding collective armbands

---
+++ @SurvivalistsRetextures/Addons/Collective_Armbands/config.cpp
@@ -0,0 +1,115 @@
+class CfgPatches
+{
+	class Collective_Armbands
+	{
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Characters"
+		};
+	};
+};
+class CfgVehicles
+{
+  class Armband_ColorBase;
+
+  class Collective_Armband_Blue: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="blue";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_blue_co.paa",
+			"Collective_Armbands\data\collective_armbend_blue_co.paa",
+			"Collective_Armbands\data\collective_armbend_blue_co.paa",
+			"Collective_Armbands\data\collective_armbend_blue_co.paa",
+			"Collective_Armbands\data\collective_armbend_blue_co.paa"
+		};
+	};
+  class Collective_Armband_Green: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="green";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_green_co.paa",
+			"Collective_Armbands\data\collective_armbend_green_co.paa",
+			"Collective_Armbands\data\collective_armbend_green_co.paa",
+			"Collective_Armbands\data\collective_armbend_green_co.paa",
+			"Collective_Armbands\data\collective_armbend_green_co.paa"
+		};
+	};
+  class Collective_Armband_Red: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="red";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_red_co.paa",
+			"Collective_Armbands\data\collective_armbend_red_co.paa",
+			"Collective_Armbands\data\collective_armbend_red_co.paa",
+			"Collective_Armbands\data\collective_armbend_red_co.paa",
+			"Collective_Armbands\data\collective_armbend_red_co.paa"
+		};
+	};
+  class Collective_Armband_White: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="white";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_white_co.paa",
+			"Collective_Armbands\data\collective_armbend_white_co.paa",
+			"Collective_Armbands\data\collective_armbend_white_co.paa",
+			"Collective_Armbands\data\collective_armbend_white_co.paa",
+			"Collective_Armbands\data\collective_armbend_white_co.paa"
+		};
+	};
+  class Collective_Armband_Pink: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="pink";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_pink_co.paa",
+			"Collective_Armbands\data\collective_armbend_pink_co.paa",
+			"Collective_Armbands\data\collective_armbend_pink_co.paa",
+			"Collective_Armbands\data\collective_armbend_pink_co.paa",
+			"Collective_Armbands\data\collective_armbend_pink_co.paa"
+		};
+	};
+  class Collective_Armband_Rainbow: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="Rainbow";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_rainbow_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbow_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbow_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbow_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbow_co.paa"
+		};
+	};
+  class Collective_Armband_RainbowMax: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="Rainbowmax";
+		hiddenSelectionsTextures[]=
+		{
+			"Collective_Armbands\data\collective_armbend_rainbowmax_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbowmax_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbowmax_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbowmax_co.paa",
+			"Collective_Armbands\data\collective_armbend_rainbowmax_co.paa"
+		};
+	};
+};
