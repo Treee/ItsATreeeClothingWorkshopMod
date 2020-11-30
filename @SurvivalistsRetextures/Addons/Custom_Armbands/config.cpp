01e83a73ff213aedc45620df43dad076f9570c8e
Treee <jonathon.lindemann@gmail.com>
2020-11-30 12:43:04 -0700

    updating armbands file structure and ignored paa files. forcing anyone who clones the repo to convert any image they need to use. done for file size purposes. no need to store generated files, specially when it exceeds 100mb

---
+++ @SurvivalistsRetextures/Addons/Custom_Armbands/config.cpp
@@ -0,0 +1,144 @@
+class CfgPatches
+{
+	class IAT_Custom_Armbands
+	{
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Characters"
+		};
+	};
+};
+class CfgVehicles
+{
+  class Armband_ColorBase;
+
+  class Collective_Armband_Blue: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="blue";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa"
+		};
+	};
+  class Collective_Armband_Green: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="green";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_green_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_green_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_green_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_green_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_green_co.paa"
+		};
+	};
+  class Collective_Armband_Red: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="red";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_red_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_red_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_red_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_red_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_red_co.paa"
+		};
+	};
+  class Collective_Armband_White: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="white";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_white_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_white_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_white_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_white_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_white_co.paa"
+		};
+	};
+  class Collective_Armband_Pink: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="pink";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_pink_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_pink_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_pink_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_pink_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_pink_co.paa"
+		};
+	};
+  class Collective_Armband_Rainbow: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="Rainbow";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbow_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbow_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbow_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbow_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbow_co.paa"
+		};
+	};
+  class Collective_Armband_RainbowMax: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="Rainbowmax";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbowmax_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbowmax_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbowmax_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbowmax_co.paa",
+			"IAT_Custom_Armbands\data\collective\collective_armbend_rainbowmax_co.paa"
+		};
+	};
+
+  class Settlers_Of_Portland_Deer_Armband_Blue: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="blue";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_deer_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_deer_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_deer_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_deer_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_deer_armbend_blue_co.paa"
+		};
+	};
+  class Settlers_Of_Portland_Motto_Armband_Blue: Armband_ColorBase
+	{
+		scope=2;
+		visibilityModifier=0.85000002;
+		color="blue";
+		hiddenSelectionsTextures[]=
+		{
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_motto_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_motto_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_motto_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_motto_armbend_blue_co.paa",
+			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_motto_armbend_blue_co.paa"
+		};
+	};
+};
