49da2a079476a48e3dfff40c4bd24def2ddd3d2c
Treee <jonathon.lindemann@gmail.com>
2020-12-04 18:39:33 -0700

    updating armbands to use masses double armband

--- @SurvivalistsRetextures/Addons/Custom_Armbands/config.cpp
+++ @SurvivalistsRetextures/Addons/Custom_Armbands/config.cpp
@@ -12,9 +12,9 @@ class CfgPatches
 };
 class CfgVehicles
 {
-  class Armband_ColorBase;
+  class MassArmbandDouble_Colorbase;
 
-  class Collective_Armband_Blue: Armband_ColorBase
+  class Collective_Armband_Blue: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -28,21 +28,21 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\collective\collective_armbend_blue_co.paa"
 		};
 	};
-  class Freedom_Armband_Grey: Armband_ColorBase
+  class PALF_Armband_Grey: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
 		color="grey";
 		hiddenSelectionsTextures[]=
 		{
-			"IAT_Custom_Armbands\data\freedom\freedom_armbend_co.paa",
-			"IAT_Custom_Armbands\data\freedom\freedom_armbend_co.paa",
-			"IAT_Custom_Armbands\data\freedom\freedom_armbend_co.paa",
-			"IAT_Custom_Armbands\data\freedom\freedom_armbend_co.paa",
-			"IAT_Custom_Armbands\data\freedom\freedom_armbend_co.paa"
+			"IAT_Custom_Armbands\data\palf\palf_armbend_co.paa",
+			"IAT_Custom_Armbands\data\palf\palf_armbend_co.paa",
+			"IAT_Custom_Armbands\data\palf\palf_armbend_co.paa",
+			"IAT_Custom_Armbands\data\palf\palf_armbend_co.paa",
+			"IAT_Custom_Armbands\data\palf\palf_armbend_co.paa"
 		};
 	};
-  class Generic_Bandit_Armband_Black: Armband_ColorBase
+  class Generic_Bandit_Armband_Black: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -56,7 +56,7 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\generic_bandit\bandits_armbend_co.paa"
 		};
 	};
-  class Settlers_Of_Portland_Deer_Armband_Blue: Armband_ColorBase
+  class Settlers_Of_Portland_Deer_Armband_Blue: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -70,7 +70,7 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\settlers_of_portland\settlers_of_portland_armbend_blue_co.paa"
 		};
 	};
-  class Reaperz_Squad_Armband_Purple: Armband_ColorBase
+  class Reaperz_Squad_Armband_Purple: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -84,7 +84,7 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\reaperz_squad\reaperz_squad_armbend_co.paa"
 		};
 	};
-  class Sundown_Armband_Black: Armband_ColorBase
+  class Sundown_Armband_Black: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -98,7 +98,7 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\sundown\sundown_armbend_co.paa"
 		};
 	};
-  class The_Marketplace_Armband_Black: Armband_ColorBase
+  class The_Marketplace_Armband_Black: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -112,21 +112,21 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\the_marketplace\the_marketplace_armbend_co.paa"
 		};
 	};
-  class The_New_Order_Armband_Black: Armband_ColorBase
+  class The_New_Order_Armband_Black: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
 		color="black";
 		hiddenSelectionsTextures[]=
 		{
-			"IAT_Custom_Armbands\data\the_new_order\the_new_order_v2_armbend_co.paa",
-			"IAT_Custom_Armbands\data\the_new_order\the_new_order_v2_armbend_co.paa",
-			"IAT_Custom_Armbands\data\the_new_order\the_new_order_v2_armbend_co.paa",
-			"IAT_Custom_Armbands\data\the_new_order\the_new_order_v2_armbend_co.paa",
-			"IAT_Custom_Armbands\data\the_new_order\the_new_order_v2_armbend_co.paa"
+			"IAT_Custom_Armbands\data\the_new_order\the_new_order_armbend_co.paa",
+			"IAT_Custom_Armbands\data\the_new_order\the_new_order_armbend_co.paa",
+			"IAT_Custom_Armbands\data\the_new_order\the_new_order_armbend_co.paa",
+			"IAT_Custom_Armbands\data\the_new_order\the_new_order_armbend_co.paa",
+			"IAT_Custom_Armbands\data\the_new_order\the_new_order_armbend_co.paa"
 		};
 	};
-  class Wardens_Armband_Black: Armband_ColorBase
+  class Wardens_Armband_Black: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
@@ -140,7 +140,7 @@ class CfgVehicles
 			"IAT_Custom_Armbands\data\wardens\wardens_armbend_co.paa"
 		};
 	};
-  class Wolfpack_Armband_Grey: Armband_ColorBase
+  class Wolfpack_Armband_Grey: MassArmbandDouble_Colorbase
 	{
 		scope=2;
 		visibilityModifier=0.85000002;
