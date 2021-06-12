8acb586f43dee4c84ac943d8ccc8ba6a8d1e4166
Treee <itsatreee.twitch@gmail.com>
2021-06-11 20:04:28 -0600

    adding jumping override for monster skin

---
+++ @SRP_Tree_Mods/Addons/SRP_Tree_Mods/scripts/4_world/entities/srp_dayzplayerimplementjumpclimb.c
@@ -0,0 +1,19 @@
+modded class DayZPlayerImplementJumpClimb
+{
+	override void Jump()
+	{
+		m_bIsJumpInProgress = true;
+		m_Player.SetFallYDiff(m_Player.GetPosition()[1]);
+
+		m_Player.OnJumpStart();
+    
+    EntityAI maskAttachment = m_Player.FindAttachmentBySlotName("Armband");
+    if (maskAttachment && maskAttachment.GetType() == "DUB_Monster") {
+      m_Player.StartCommand_Fall(15);
+    }
+    else {
+		  m_Player.StartCommand_Fall(2.6);  // default
+    }
+		m_Player.StopHandEvent();
+	}
+}
