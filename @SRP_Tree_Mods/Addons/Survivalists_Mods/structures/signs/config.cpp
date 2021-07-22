d9532310a93ff414805febde2a79210e2aa799cc
Treee <itsatreee.twitch@gmail.com>
2021-07-22 16:07:12 -0600

    signs are updated

--- @SRP_Tree_Mods/Addons/Survivalists_Mods/structures/signs/config.cpp
+++ @SRP_Tree_Mods/Addons/Survivalists_Mods/structures/signs/config.cpp
@@ -46,48 +46,5 @@ class CfgVehicles
 		{
 			"Survivalists_Mods\structures\signs\data\nationalforestsign_sancutary_co.paa"
 		};
-	};
-
-  class land_srp_townsign_base: HouseNoDestruct  // new
-	{
-		scope=1;
-		model="Survivalists_Mods\structures\signs\SRP_TownSign.p3d";
-    hiddenSelections[]=
-		{
-			"zbytek"
-		};
-    hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\structures\signs\data\srp_townsign_co.paa"
-		};
-    hiddenSelectionsMaterials[]=
-		{
-			"Survivalists_Mods\structures\signs\data\srp_townsign.rvmat"
-		};
-	};
-  class land_srp_townsign_bastion: land_srp_townsign_base  // new
-	{
-		scope=1;
-    hiddenSelections[]=
-		{
-			"zbytek"
-		};
-    hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\structures\signs\data\srp_townsign_bastion_co.paa"
-		};
-	};
-  class land_srp_townsign_darkwatertavern: land_srp_townsign_base  // new
-	{
-		scope=1;
-    hiddenSelections[]=
-		{
-			"zbytek"
-		};
-    hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\structures\signs\data\srp_townsign_darkwater_co.paa"
-		};
-	};
-  
+	};  
 };
