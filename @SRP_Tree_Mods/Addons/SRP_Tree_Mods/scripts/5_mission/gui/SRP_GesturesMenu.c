5668af7bf70e01b95d63dfd90e18aeeec27c3fd8
Treee <itsatreee.twitch@gmail.com>
2021-08-08 09:50:04 -0600

    removing dab emote

--- @SRP_Tree_Mods/Addons/SRP_Tree_Mods/scripts/5_mission/gui/SRP_GesturesMenu.c
+++ @SRP_Tree_Mods/Addons/SRP_Tree_Mods/scripts/5_mission/gui/SRP_GesturesMenu.c
@@ -1,6 +1,6 @@
 modded class GesturesMenu
 {
-  override void GetGestureItems(out ref array<ref GestureMenuItem> gesture_items, GestureCategories category)
+  override void GetGestureItems(out ref array<ref GestureMenuItem> gesture_items, GestureCategories category)z
   {
     super.GetGestureItems(gesture_items, category);
     switch (category)
@@ -8,6 +8,37 @@ modded class GesturesMenu
       case GestureCategories.CATEGORIES:
       {
         gesture_items.Insert( new GestureMenuItem( GestureCategories.CATEGORY_5, "Custom", 	GestureCategories.CATEGORIES ) );
+        break;
+      }
+      case GestureCategories.CATEGORY_2:
+      {
+        // reset category 2 and remove the dab emote
+        gesture_items.Clear();
+        
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_LOOKATME, 	"#STR_USRACT_ID_EMOTE_LOOKATME", 	GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_TAUNT, 		"#STR_USRACT_ID_EMOTE_TAUNT", 		GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_TAUNTELBOW, "#STR_USRACT_ID_EMOTE_TAUNTELBOW", 	GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_THROAT, 	"#STR_USRACT_ID_EMOTE_THROAT", 		GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_TAUNTTHINK, "#STR_USRACT_ID_EMOTE_TAUNTTHINK", 	GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_FACEPALM, 	"#STR_USRACT_ID_EMOTE_FACEPALM", 	GestureCategories.CATEGORY_2 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_THUMBDOWN, 	"#STR_USRACT_ID_EMOTE_THUMBDOWN", 	GestureCategories.CATEGORY_2 ) );
+        break;
+      }
+      case GestureCategories.CATEGORY_3:
+      {
+        // reset category 2 and remove the dab emote
+        gesture_items.Clear();
+        
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_DANCE, 		"#STR_USRACT_ID_EMOTE_DANCE", 		GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_LYINGDOWN, 	"#STR_USRACT_ID_EMOTE_LYINGDOWN", 	GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_SOS, 		"#STR_USRACT_ID_EMOTE_SOS", 		GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_RPS, 		"#STR_USRACT_ID_EMOTE_RPS", 		GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_SITA, 		"#STR_USRACT_ID_EMOTE_SITA", 		GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_SITB, 		"#STR_USRACT_ID_EMOTE_SITB", 		GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_SUICIDE, 	"#STR_USRACT_ID_EMOTE_SUICIDE", 	GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_CAMPFIRE, 	"#STR_USRACT_ID_EMOTE_CAMPFIRE", 	GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_SURRENDER, 	"#STR_USRACT_ID_EMOTE_SURRENDER", 	GestureCategories.CATEGORY_3 ) );
+        gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_VOMIT, 		"#STR_USRACT_ID_EMOTE_VOMIT", 		GestureCategories.CATEGORY_3 ) );
         break;
       }
       case GestureCategories.CATEGORY_5: //STR_USRACT_EMOTE_CATEGORY_TAUNTS
