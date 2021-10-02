d71446d5e249fcc78d9ff931b582ee76f00c2ac1
Treee <itsatreee.twitch@gmail.com>
2021-10-02 08:47:20 -0600

    mutant heal in biozone areas

--- @SRP_Tree_Mods/Addons/Survivalists_Mods/scripts/4_world/modifiers/modded_areaexposure.c
+++ @SRP_Tree_Mods/Addons/Survivalists_Mods/scripts/4_world/modifiers/modded_areaexposure.c
@@ -5,27 +5,42 @@ modded class AreaExposureMdfr
     // Print("AreaExposureMdfr::OnActivate::Start");
     if (player && player.SRPIgnoreContaminatedArea())
     {
-      Print("Mutant: return from activation");
+      if (player.GetSingleAgentCount(DUB_MutantAgent.MUTANT_AGENT) >= 9600)
+      {
+        // Print("OnActivate::Turn on biohazard heal");
+        player.GetModifiersManager().ActivateModifier(DUB_Modifiers.MDF_BioHazardHeal);
+      }
+      // Print("OnActivate::Mutant: return from activation");
       return;
     }
     else
     {
-      Print("call super");
+      // Print("call super");
       super.OnActivate(player);
     }
 	}
+
+  override void OnDeactivate(PlayerBase player)
+	{
+    // Print("AreaExposureMdfr::OnDeactivate::Start");
+    if (player && player.GetSingleAgentCount(DUB_MutantAgent.MUTANT_AGENT) >= 9600)
+    {
+      // Print("AreaExposureMdfr::OnDeactivate:: we are a level 3 mutant");
+      player.GetModifiersManager().DeactivateModifier(DUB_Modifiers.MDF_BioHazardHeal);
+    }	
+  }
 
   override void OnTick(PlayerBase player, float deltaT)
 	{
     // Print("AreaExposureMdfr::OnTick::Start");
     if (player && player.SRPIgnoreContaminatedArea())
     {
-      Print("Mutant: return from activation");
+      // Print("OnTick::Mutant: return from activation");
       return;
     }
     else
     {
-      Print("call super");
+      // Print("call super");
       super.OnTick(player, deltaT);
     }  
   }
