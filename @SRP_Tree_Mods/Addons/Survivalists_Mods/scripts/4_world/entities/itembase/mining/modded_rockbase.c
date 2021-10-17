5992de612b9649a8469ec0306ca382066d0b9103
Treee <itsatreee.twitch@gmail.com>
2021-10-16 20:15:03 -0600

    refactored the mining logic to be easier to add new stuff

--- @SRP_Tree_Mods/Addons/Survivalists_Mods/scripts/4_world/entities/itembase/mining/modded_rockbase.c
+++ @SRP_Tree_Mods/Addons/Survivalists_Mods/scripts/4_world/entities/itembase/mining/modded_rockbase.c
@@ -1,36 +1,22 @@
-modded class Static_stone1
+modded class RockBase
 {
-	override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
+  float platinumChance = 0.99;
+  float goldChance = 0.99;
+  float ironChance = 0.99;
+  float copperChance = 0.99;
+  float tinChance = 0.99;
+
+  override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
 	{
 		if ( item && item.KindOf("Pickaxe") )
 		{
 			output_map.Insert("Stone",2);
-      float chance = Math.RandomFloat(0,1);
-      // 5% chance for platinum
-      if (chance >= 0.95)
+      array<string> stoneChunks = GetMiningStoneChunks();
+      if (stoneChunks && stoneChunks.Count() > 0)
       {
-			  output_map.Insert("SRP_Mining_StoneChunk_Platinum", Math.RandomIntInclusive(1,2));
-      }
-      // 10% chance for gold
-      else if (chance >= 0.90)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Gold", Math.RandomIntInclusive(1,2));			  
-      }
-      // 15% chance for iron
-      else if (chance >= 0.85)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Iron", Math.RandomIntInclusive(1,2));			  
-      }
-      // 20% chance for tin and copper
-      else if (chance >= 0.80)
-      {
-        if (Math.RandomIntInclusive(0,1) == 0)
+        for (int i = 0; i < stoneChunks.Count(); i++)
         {
-			    output_map.Insert("SRP_Mining_StoneChunk_Copper", Math.RandomIntInclusive(1,2));			  
-        }
-        else
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Tin", Math.RandomIntInclusive(1,2));			  
+          output_map.Insert(stoneChunks.Get(i), Math.RandomIntInclusive(1,2));
         }
       }
 		}
@@ -42,6 +28,51 @@ modded class Static_stone1
 		{
 			output_map.Insert("Stone",1);
 		}
+	}
+
+  //values between 0-1
+  array<string> GetMiningStoneChunks()
+  {
+    array<string> stoneChunks = new array<string>;
+    float chance = Math.RandomFloatInclusive(0,1);
+    if (chance >= platinumChance)
+    {
+      stoneChunks.Insert("SRP_Mining_StoneChunk_Platinum");      
+    }
+    chance = Math.RandomFloatInclusive(0,1);
+    if (chance >= goldChance)
+    {
+      stoneChunks.Insert("SRP_Mining_StoneChunk_Gold");      
+    }
+    chance = Math.RandomFloatInclusive(0,1);
+    if (chance >= ironChance)
+    {
+      stoneChunks.Insert("SRP_Mining_StoneChunk_Iron");
+    }
+    chance = Math.RandomFloatInclusive(0,1);
+    if (chance >= copperChance)
+    {
+      stoneChunks.Insert("SRP_Mining_StoneChunk_Copper");
+    }
+    chance = Math.RandomFloatInclusive(0,1);
+    if (chance >= tinChance)
+    {
+      stoneChunks.Insert("SRP_Mining_StoneChunk_Tin");          
+    }
+    return stoneChunks;
+  }
+};
+
+modded class Static_stone1
+{
+	override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
+	{
+    platinumChance = 0.95;
+    goldChance = 0.90;
+    ironChance = 0.85;
+    copperChance = 0.80;
+    tinChance = 0.80;
+    super.GetMaterialAndQuantityMap(item, output_map);		
 	}
 };
 
@@ -49,46 +80,12 @@ modded class Static_stone2
 {
   override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
 	{
-		if ( item && item.KindOf("Pickaxe") )
-		{
-			output_map.Insert("Stone",2);
-      float chance = Math.RandomFloat(0,1);
-      // 5% chance for platinum
-      if (chance >= 0.95)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Gold", Math.RandomIntInclusive(1,2));			  
-      }
-      // 10% chance for gold
-      else if (chance >= 0.90)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Platinum", Math.RandomIntInclusive(1,2));
-      }
-      // 15% chance for iron
-      else if (chance >= 0.85)
-      {
-        if (Math.RandomIntInclusive(0,1) == 0)
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Copper", Math.RandomIntInclusive(1,2));			  
-        }
-        else
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Tin", Math.RandomIntInclusive(1,2));			  
-        }
-      }
-      // 20% chance for tin and copper
-      else if (chance >= 0.80)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Iron", Math.RandomIntInclusive(1,2));			  
-      }
-		}
-		else if ( item && item.KindOf("SledgeHammer") )
-		{
-			output_map.Insert("Stone",1);
-		}
-		else if ( item )
-		{
-			output_map.Insert("Stone",1);
-		}
+    platinumChance = 0.80;
+    goldChance = 0.75;
+    ironChance = 0.90;
+    copperChance = 0.95;
+    tinChance = 0.80;
+    super.GetMaterialAndQuantityMap(item, output_map);		
 	}
 };
 
@@ -96,139 +93,35 @@ modded class Static_stone3
 {
   override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
 	{
-		if ( item && item.KindOf("Pickaxe") )
-		{
-			output_map.Insert("Stone",2);
-      float chance = Math.RandomFloat(0,1);
-      // 5% chance for platinum
-      if (chance >= 0.98)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Platinum", Math.RandomIntInclusive(1,2));
-      }
-      // 10% chance for gold
-      else if (chance >= 0.95)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Gold", Math.RandomIntInclusive(1,2));			  
-      }
-      // 15% chance for iron
-      else if (chance >= 0.90)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Iron", Math.RandomIntInclusive(1,2));			  
-      }
-      // 20% chance for tin and copper
-      else if (chance >= 0.87)
-      {
-        if (Math.RandomIntInclusive(0,1) == 0)
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Copper", Math.RandomIntInclusive(1,2));			  
-        }
-        else
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Tin", Math.RandomIntInclusive(1,2));			  
-        }
-      }
-		}
-		else if ( item && item.KindOf("SledgeHammer") )
-		{
-			output_map.Insert("Stone",1);
-		}
-		else if ( item )
-		{
-			output_map.Insert("Stone",1);
-		}
+    platinumChance = 0.90;
+    goldChance = 0.95;
+    ironChance = 0.75;
+    copperChance = 0.85;
+    tinChance = 0.85;
+    super.GetMaterialAndQuantityMap(item, output_map);		
 	}
-
 };
 modded class Static_stone4
 {
   override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
 	{
-		if ( item && item.KindOf("Pickaxe") )
-		{
-			output_map.Insert("Stone",2);
-      float chance = Math.RandomFloat(0,1);
-      // 5% chance for platinum
-      if (chance >= 0.95)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Platinum", Math.RandomIntInclusive(1,2));
-      }
-      // 10% chance for gold
-      else if (chance >= 0.93)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Gold", Math.RandomIntInclusive(1,2));			  
-      }
-      // 15% chance for iron
-      else if (chance >= 0.90)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Iron", Math.RandomIntInclusive(1,2));			  
-      }
-      // 20% chance for tin and copper
-      else if (chance >= 0.88)
-      {
-        if (Math.RandomIntInclusive(0,1) == 0)
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Copper", Math.RandomIntInclusive(1,2));			  
-        }
-        else
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Tin", Math.RandomIntInclusive(1,2));			  
-        }
-      }
-		}
-		else if ( item && item.KindOf("SledgeHammer") )
-		{
-			output_map.Insert("Stone",1);
-		}
-		else if ( item )
-		{
-			output_map.Insert("Stone",1);
-		}
+    platinumChance = 0.95;
+    goldChance = 0.90;
+    ironChance = 0.80;
+    copperChance = 0.90;
+    tinChance = 0.90;
+    super.GetMaterialAndQuantityMap(item, output_map);		
 	}
-
 };
 modded class Static_stone5
 {
   override void GetMaterialAndQuantityMap(ItemBase item, out map<string,int> output_map)
 	{
-		if ( item && item.KindOf("Pickaxe") )
-		{
-			output_map.Insert("Stone",2);
-      float chance = Math.RandomFloat(0,1);
-      // 5% chance for platinum
-      if (chance >= 0.95)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Platinum", Math.RandomIntInclusive(1,2));
-      }
-      // 10% chance for gold
-      else if (chance >= 0.90)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Gold", Math.RandomIntInclusive(1,2));			  
-      }
-      // 15% chance for iron
-      else if (chance >= 0.85)
-      {
-			  output_map.Insert("SRP_Mining_StoneChunk_Iron", Math.RandomIntInclusive(1,2));			  
-      }
-      // 20% chance for tin and copper
-      else if (chance >= 0.80)
-      {
-        if (Math.RandomIntInclusive(0,1) == 0)
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Copper", Math.RandomIntInclusive(1,2));			  
-        }
-        else
-        {
-			    output_map.Insert("SRP_Mining_StoneChunk_Tin", Math.RandomIntInclusive(1,2));			  
-        }
-      }
-		}
-		else if ( item && item.KindOf("SledgeHammer") )
-		{
-			output_map.Insert("Stone",1);
-		}
-		else if ( item )
-		{
-			output_map.Insert("Stone",1);
-		}
+    platinumChance = 0.95;
+    goldChance = 0.95;
+    ironChance = 0.95;
+    copperChance = 0.95;
+    tinChance = 0.95;
+    super.GetMaterialAndQuantityMap(item, output_map);		
 	}
 };
