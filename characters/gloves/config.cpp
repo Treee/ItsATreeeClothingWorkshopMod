04f818726318b65302e7d5715b9c85e4cebf30ca
Treee <itsatreee.twitch@gmail.com>
2021-12-07 14:30:24 -0700

    moving glove retextures to other mod

--- characters/gloves/config.cpp
+++ characters/gloves/config.cpp
@@ -15,96 +15,17 @@ class CfgPatches
 };
 class CfgVehicles
 {
-  class Clothing;
-  class OMNOGloves_ColorBase;
-  class WorkingGloves_ColorBase;
-  
+  class Clothing;  
 
   //-------------------------------------BASE GAME OVERRIDE
   class NBCGloves_ColorBase: Clothing
 	{
-		class Protection {
+    heatIsolation=0.6;
+		repairableWithKits[]={5,6}; // duct tape and repair kit
+		class Protection 
+    {
       biological=1;
     }
 	};
 
-  // ------------------------ Base Game Retextures
-  class WorkingGloves_White: WorkingGloves_ColorBase
-	{
-		scope=2;
-		visibilityModifier=0.89999998;
-		hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_white_co.paa",
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_white_co.paa",
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_white_co.paa"
-		};
-	};
-  class WorkingGloves_Red: WorkingGloves_ColorBase
-	{
-		scope=2;
-		visibilityModifier=0.89999998;
-		hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_red_co.paa",
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_red_co.paa",
-			"Survivalists_Mods\characters\gloves\data\WorkingGloves_red_co.paa"
-		};
-	};
-
-	class SRP_NBCGloves_Blue: NBCGloves_ColorBase  // BP2_NBCGlovesBlue
-	{
-		scope=2;
-    colr="blue";
-		repairableWithKits[]={5,6}; // duct tape and rapaier kit
-		hiddenSelections[]=
-		{
-			"camoGround",
-			"camoMale",
-			"camoFemale"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\characters\gloves\data\nbc_gloves_blue_co.paa",
-			"Survivalists_Mods\characters\gloves\data\nbc_gloves_blue_co.paa",
-			"Survivalists_Mods\characters\gloves\data\nbc_gloves_blue_co.paa"
-		};
-	};
-  
-	class SRP_OMNOGloves_Green: OMNOGloves_ColorBase  // BP2_Denglove_green
-	{
-		scope=2;
-    color="green";
-    hiddenSelections[]=
-		{
-			"camoGround",
-			"camoMale",
-			"camoFemale",
-			"personality"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\characters\gloves\data\Denglove_green.paa",
-			"Survivalists_Mods\characters\gloves\data\Denglove_green.paa",
-			"Survivalists_Mods\characters\gloves\data\Denglove_green.paa"
-		};
-	};
-	class SRP_OMNOGloves_Black: OMNOGloves_ColorBase  // Denglove
-	{
-		scope=2;
-    color="black";
-    hiddenSelections[]=
-		{
-			"camoGround",
-			"camoMale",
-			"camoFemale",
-			"personality"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"Survivalists_Mods\characters\gloves\data\denglove.paa",
-			"Survivalists_Mods\characters\gloves\data\denglove.paa",
-			"Survivalists_Mods\characters\gloves\data\denglove.paa"
-		};
-	};
 };
