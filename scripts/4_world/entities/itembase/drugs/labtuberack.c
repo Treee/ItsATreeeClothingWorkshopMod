f9f6bb854400c5cbf9825bb9f4e0ad66d94801e4
Treee <itsatreee.twitch@gmail.com>
2021-12-08 15:31:44 -0700

    scrambling drug recipes

--- scripts/4_world/entities/itembase/drugs/labtuberack.c
+++ scripts/4_world/entities/itembase/drugs/labtuberack.c
@@ -24,18 +24,18 @@ class SRP_LabTubeRack extends ItemBase{
   }
 
   bool IsCocainePuzzleSolved() {
-    bool slot1 = IsSlotFilledWithColor("TestTube1", "green") || IsSlotFilledWithColor("TestTube1", "clear") || IsSlotFilledWithColor("TestTube1", "green");
-    bool slot2 = IsSlotFilledWithColor("TestTube2", "blue") || IsSlotFilledWithColor("TestTube2", "red") || IsSlotFilledWithColor("TestTube2", "clear");
+    bool slot1 = IsSlotFilledWithColor("TestTube5", "green") || IsSlotFilledWithColor("TestTube5", "clear") || IsSlotFilledWithColor("TestTube5", "green");
+    bool slot2 = IsSlotFilledWithColor("TestTube3", "blue") || IsSlotFilledWithColor("TestTube3", "red") || IsSlotFilledWithColor("TestTube3", "clear");
     bool slot4 = IsSlotFilledWithColor("TestTube4", "red") || IsSlotFilledWithColor("TestTube4", "blue") || IsSlotFilledWithColor("TestTube4", "green");
-    bool slot5 = IsSlotFilledWithColor("TestTube5", "clear") || IsSlotFilledWithColor("TestTube5", "green") || IsSlotFilledWithColor("TestTube5", "yellow");
+    bool slot5 = IsSlotFilledWithColor("TestTube1", "clear") || IsSlotFilledWithColor("TestTube1", "green") || IsSlotFilledWithColor("TestTube1", "yellow");
     return slot1 && slot2 && slot4 && slot5;
   }
 
   bool IsMethPuzzleSolved() {
-    bool slot1 = IsSlotFilledWithColor("TestTube1", "blue") || IsSlotFilledWithColor("TestTube1", "yellow") || IsSlotFilledWithColor("TestTube1", "clear");
-    bool slot2 = IsSlotFilledWithColor("TestTube2", "yellow") || IsSlotFilledWithColor("TestTube2", "blue") || IsSlotFilledWithColor("TestTube2", "red");
+    bool slot1 = IsSlotFilledWithColor("TestTube5", "blue") || IsSlotFilledWithColor("TestTube5", "yellow") || IsSlotFilledWithColor("TestTube5", "clear");
+    bool slot2 = IsSlotFilledWithColor("TestTube3", "yellow") || IsSlotFilledWithColor("TestTube3", "blue") || IsSlotFilledWithColor("TestTube3", "red");
     bool slot4 = IsSlotFilledWithColor("TestTube4", "yellow") || IsSlotFilledWithColor("TestTube4", "red") || IsSlotFilledWithColor("TestTube4", "green");
-    bool slot5 = IsSlotFilledWithColor("TestTube5", "blue") || IsSlotFilledWithColor("TestTube5", "red") || IsSlotFilledWithColor("TestTube5", "yellow");
+    bool slot5 = IsSlotFilledWithColor("TestTube1", "blue") || IsSlotFilledWithColor("TestTube1", "red") || IsSlotFilledWithColor("TestTube1", "yellow");
     return slot1 && slot2 && slot4 && slot5;
   }
 
@@ -43,28 +43,28 @@ class SRP_LabTubeRack extends ItemBase{
     return IsSkullAcidPuzzleSolved() || IsSmileyAcidPuzzleSolved();
   }
   bool IsSmileyAcidPuzzleSolved() {
-    bool slot1 = IsSlotFilledWithColor("TestTube1", "blue") || IsSlotFilledWithColor("TestTube1", "yellow") || IsSlotFilledWithColor("TestTube1", "clear");
-    bool slot2 = IsSlotFilledWithColor("TestTube2", "red") || IsSlotFilledWithColor("TestTube2", "red") || IsSlotFilledWithColor("TestTube2", "green");
-    bool slot3 = IsSlotFilledWithColor("TestTube3", "yellow") || IsSlotFilledWithColor("TestTube3", "blue") || IsSlotFilledWithColor("TestTube3", "red");
+    bool slot1 = IsSlotFilledWithColor("TestTube5", "blue") || IsSlotFilledWithColor("TestTube5", "yellow") || IsSlotFilledWithColor("TestTube5", "clear");
+    bool slot2 = IsSlotFilledWithColor("TestTube3", "red") || IsSlotFilledWithColor("TestTube3", "red") || IsSlotFilledWithColor("TestTube3", "green");
+    bool slot3 = IsSlotFilledWithColor("TestTube2", "yellow") || IsSlotFilledWithColor("TestTube2", "blue") || IsSlotFilledWithColor("TestTube2", "red");
     bool slot4 = IsSlotFilledWithColor("TestTube4", "clear") || IsSlotFilledWithColor("TestTube4", "yellow") || IsSlotFilledWithColor("TestTube4", "blue");
-    bool slot5 = IsSlotFilledWithColor("TestTube5", "green") || IsSlotFilledWithColor("TestTube5", "red") || IsSlotFilledWithColor("TestTube5", "yellow");
+    bool slot5 = IsSlotFilledWithColor("TestTube1", "green") || IsSlotFilledWithColor("TestTube1", "red") || IsSlotFilledWithColor("TestTube1", "yellow");
     bool slot6 = IsSlotFilledWithColor("TestTube6", "clear") || IsSlotFilledWithColor("TestTube6", "clear") || IsSlotFilledWithColor("TestTube6", "clear");
     return slot1 && slot2 && slot3 && slot4 && slot5 && slot6;
   }
   bool IsSkullAcidPuzzleSolved() {
-    bool slot6 = IsSlotFilledWithColor("TestTube1", "clear") || IsSlotFilledWithColor("TestTube1", "green") || IsSlotFilledWithColor("TestTube1", "red");
-    bool slot5 = IsSlotFilledWithColor("TestTube2", "blue") || IsSlotFilledWithColor("TestTube2", "clear") || IsSlotFilledWithColor("TestTube2", "yellow");
-    bool slot4 = IsSlotFilledWithColor("TestTube3", "red") || IsSlotFilledWithColor("TestTube3", "yellow") || IsSlotFilledWithColor("TestTube3", "blue");
+    bool slot6 = IsSlotFilledWithColor("TestTube5", "clear") || IsSlotFilledWithColor("TestTube5", "green") || IsSlotFilledWithColor("TestTube5", "red");
+    bool slot5 = IsSlotFilledWithColor("TestTube3", "blue") || IsSlotFilledWithColor("TestTube3", "clear") || IsSlotFilledWithColor("TestTube3", "yellow");
+    bool slot4 = IsSlotFilledWithColor("TestTube2", "red") || IsSlotFilledWithColor("TestTube2", "yellow") || IsSlotFilledWithColor("TestTube2", "blue");
     bool slot3 = IsSlotFilledWithColor("TestTube4", "blue") || IsSlotFilledWithColor("TestTube4", "clear") || IsSlotFilledWithColor("TestTube4", "red");
-    bool slot2 = IsSlotFilledWithColor("TestTube5", "yellow") || IsSlotFilledWithColor("TestTube5", "green") || IsSlotFilledWithColor("TestTube5", "green");
+    bool slot2 = IsSlotFilledWithColor("TestTube1", "yellow") || IsSlotFilledWithColor("TestTube1", "green") || IsSlotFilledWithColor("TestTube1", "green");
     bool slot1 = IsSlotFilledWithColor("TestTube6", "red") || IsSlotFilledWithColor("TestTube6", "blue") || IsSlotFilledWithColor("TestTube6", "red");
     return slot1 && slot2 && slot3 && slot4 && slot5 && slot6;
   }
 
   bool IsBathSaltsPuzzleSolved() {
-    bool slot5 = IsSlotFilledWithColor("TestTube2", "green") || IsSlotFilledWithColor("TestTube2", "red");
+    bool slot5 = IsSlotFilledWithColor("TestTube3", "green") || IsSlotFilledWithColor("TestTube3", "red");
     bool slot3 = IsSlotFilledWithColor("TestTube4", "clear") || IsSlotFilledWithColor("TestTube4", "red") || IsSlotFilledWithColor("TestTube4", "blue");
-    bool slot2 = IsSlotFilledWithColor("TestTube5", "blue") || IsSlotFilledWithColor("TestTube5", "red") || IsSlotFilledWithColor("TestTube5", "yellow");
+    bool slot2 = IsSlotFilledWithColor("TestTube1", "blue") || IsSlotFilledWithColor("TestTube1", "red") || IsSlotFilledWithColor("TestTube1", "yellow");
     return slot2 && slot3 && slot5;
   }
 
