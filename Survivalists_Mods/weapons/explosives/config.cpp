3948b539759da9b3cd7115ce07ed930b7ab69c55
Treee <itsatreee.twitch@gmail.com>
2021-12-12 09:24:46 -0700

    removing dynamite

--- Survivalists_Mods/weapons/explosives/config.cpp
+++ Survivalists_Mods/weapons/explosives/config.cpp
@@ -59,63 +59,64 @@ class CfgVehicles
   class Inventory_Base;
   class Grenade_Base;
 
-  class SRP_Dynamite_Stick : Inventory_Base
-  {
-		scope=2;
-		displayName="Dynamite";
-		descriptionShort="A single stick of dynamite... explosive.";
-		model="Survivalists_Mods\weapons\explosives\dynamite_stick.p3d";
-		weight=10;
-    varTemperatureMax=100;
-    itemSize[]={1,3};
-    varQuantityInit=10;
-		varQuantityMin=0;
-		varQuantityMax=10;
-    destroyOnEmpty=1;	
-		hiddenSelections[]= {"zbytek"};
-		hiddenSelectionsTextures[]= {
-      "Survivalists_Mods\weapons\explosives\data\dynamite_stick_co.paa"
-		};
-		hiddenSelectionsMaterials[]= {
-			"Survivalists_Mods\weapons\explosives\data\dynamite_stick.rvmat"
-		};		
-    class EnergyManager
-		{
-			energyAtSpawn=10;
-			energyUsagePerSecond=1;
-			updateInterval=1;
-			convertEnergyToQuantity=1;
-		};
-  };
+  // commented out because broken for now
+  // class SRP_Dynamite_Stick : Inventory_Base
+  // {
+	// 	scope=2;
+	// 	displayName="Dynamite";
+	// 	descriptionShort="A single stick of dynamite... explosive.";
+	// 	model="Survivalists_Mods\weapons\explosives\dynamite_stick.p3d";
+	// 	weight=10;
+  //   varTemperatureMax=100;
+  //   itemSize[]={1,3};
+  //   varQuantityInit=10;
+	// 	varQuantityMin=0;
+	// 	varQuantityMax=10;
+  //   destroyOnEmpty=1;	
+	// 	hiddenSelections[]= {"zbytek"};
+	// 	hiddenSelectionsTextures[]= {
+  //     "Survivalists_Mods\weapons\explosives\data\dynamite_stick_co.paa"
+	// 	};
+	// 	hiddenSelectionsMaterials[]= {
+	// 		"Survivalists_Mods\weapons\explosives\data\dynamite_stick.rvmat"
+	// 	};		
+  //   class EnergyManager
+	// 	{
+	// 		energyAtSpawn=10;
+	// 		energyUsagePerSecond=1;
+	// 		updateInterval=1;
+	// 		convertEnergyToQuantity=1;
+	// 	};
+  // };
 
-  class SRP_Dynamite_Stack : Inventory_Base
-  {
-		scope=2;
-		displayName="Dynamite Stack";
-		descriptionShort="A stack of dynamite... explosive.";
-		model="Survivalists_Mods\weapons\explosives\dynamite_stack.p3d";
-		weight=15;
-    varTemperatureMax=100;
-    itemSize[]={3,5};
-    varQuantityInit=10;
-		varQuantityMin=0;
-		varQuantityMax=10;
-    destroyOnEmpty=1;	
-		hiddenSelections[]= {"zbytek"};
-		hiddenSelectionsTextures[]= {
-      "Survivalists_Mods\weapons\explosives\data\dynamite_stack_co.paa"
-		};
-		hiddenSelectionsMaterials[]= {
-			"Survivalists_Mods\weapons\explosives\data\dynamite_stack.rvmat"
-		};		
-    class EnergyManager
-		{
-			energyAtSpawn=10;
-			energyUsagePerSecond=1;
-			updateInterval=1;
-			convertEnergyToQuantity=1;
-		};
-  };
+  // class SRP_Dynamite_Stack : Inventory_Base
+  // {
+	// 	scope=2;
+	// 	displayName="Dynamite Stack";
+	// 	descriptionShort="A stack of dynamite... explosive.";
+	// 	model="Survivalists_Mods\weapons\explosives\dynamite_stack.p3d";
+	// 	weight=15;
+  //   varTemperatureMax=100;
+  //   itemSize[]={3,5};
+  //   varQuantityInit=10;
+	// 	varQuantityMin=0;
+	// 	varQuantityMax=10;
+  //   destroyOnEmpty=1;	
+	// 	hiddenSelections[]= {"zbytek"};
+	// 	hiddenSelectionsTextures[]= {
+  //     "Survivalists_Mods\weapons\explosives\data\dynamite_stack_co.paa"
+	// 	};
+	// 	hiddenSelectionsMaterials[]= {
+	// 		"Survivalists_Mods\weapons\explosives\data\dynamite_stack.rvmat"
+	// 	};		
+  //   class EnergyManager
+	// 	{
+	// 		energyAtSpawn=10;
+	// 		energyUsagePerSecond=1;
+	// 		updateInterval=1;
+	// 		convertEnergyToQuantity=1;
+	// 	};
+  // };
 
   class SRP_DrugExplosion: Grenade_Base
 	{
