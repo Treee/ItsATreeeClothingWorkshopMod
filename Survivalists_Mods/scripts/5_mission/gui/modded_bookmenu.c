fdd85678c6d37ff56ecda3415e7210f8336dfa64
Treee <itsatreee.twitch@gmail.com>
2021-12-24 18:51:14 -0700

    hiding page ui widget that was breaking books

---
+++ Survivalists_Mods/scripts/5_mission/gui/modded_bookmenu.c
@@ -0,0 +1,17 @@
+modded class BookMenu
+{
+  override Widget Init()
+	{
+		layoutRoot = GetGame().GetWorkspace().CreateWidgets("gui/layouts/day_z_book.layout");
+		Class.CastTo(m_content, layoutRoot.FindAnyWidget("HtmlWidget"));
+		Class.CastTo(m_author, layoutRoot.FindAnyWidget("Author")); 
+		Class.CastTo(m_title, layoutRoot.FindAnyWidget("Title"));
+
+    // this line seems to be pre code bohemia has in. it broke existing book mods
+    // uncomment this when native reading books is added back.
+		// Class.CastTo(m_page, layoutRoot.FindAnyWidget("Page"));		
+		float width;
+		m_content.GetScreenSize(width, m_page_height);
+		return layoutRoot;
+	}
+};
--- Survivalists_Mods/scripts/5_mission/mission/missiongameplay.c
+++ Survivalists_Mods/scripts/5_mission/mission/missiongameplay.c
@@ -23,4 +23,4 @@ modded class MissionGameplay
       }
     }
   }
-}
+};
