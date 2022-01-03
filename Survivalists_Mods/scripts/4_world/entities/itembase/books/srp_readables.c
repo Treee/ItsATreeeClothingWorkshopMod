16b8adb64dca242d3259b7dc27e910bb74451d9c
Treee <itsatreee.twitch@gmail.com>
2022-01-03 10:33:11 -0700

    adding vanilla books

--- Survivalists_Mods/scripts/4_world/entities/itembase/books/srp_readables.c
+++ Survivalists_Mods/scripts/4_world/entities/itembase/books/srp_readables.c
@@ -1,12 +1,18 @@
-modded class ItemBook
+class SRP_ReadableBook_Colorbase extends ItemBook
 {
-	override void SetActions()
+  override void SetActions()
 	{
 		super.SetActions();
 
 		AddAction(ActionReadBook);
 	}
 };
+class SRP_ReadableScroll_Colorbase extends ItemBook
+{
+  override void SetActions()
+	{
+		super.SetActions();
 
-class SRP_ReadableBook_Colorbase extends ItemBook{};
-class SRP_ReadableScroll_Colorbase extends ItemBook{};
+		AddAction(ActionReadBook);
+	}
+};
