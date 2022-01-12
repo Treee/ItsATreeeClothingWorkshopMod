df55c24606f68530e08a7b1c783f25f65df38a74
Treee <itsatreee.twitch@gmail.com>
2022-01-11 19:49:32 -0700

    adding effects to herbalism bandages

---
+++ Survivalists_Mods/scripts/4_world/actions/continuous/modded_bandagebase.c
@@ -0,0 +1,46 @@
+modded class ActionBandageBase
+{	
+	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+    bool canAction = super.ActionCondition(player, target, item);
+    return canAction || item.IsInherited(SRP_BandageDressing);	
+	}
+	
+	void ApplyBandage( ItemBase item, PlayerBase player )
+	{	
+    if (item)
+    {
+      if (item.GetType() == "SRP_BandageDressing_Healing")
+      {
+        if( player.GetModifiersManager().IsModifierActive(eModifiers.MDF_PAINKILLERS ) )//effectively resets the timer
+        {
+          player.GetModifiersManager().DeactivateModifier( eModifiers.MDF_PAINKILLERS, false );
+        }
+        player.GetModifiersManager().ActivateModifier( eModifiers.MDF_PAINKILLERS );
+
+        if( player.GetModifiersManager().IsModifierActive(eModifiers.MDF_IMMUNITYBOOST ) )//effectively resets the timer
+        {
+          player.GetModifiersManager().DeactivateModifier( eModifiers.MDF_IMMUNITYBOOST );
+        }
+        player.GetModifiersManager().ActivateModifier( eModifiers.MDF_IMMUNITYBOOST );
+      }
+      else if (item.GetType() == "SRP_BandageDressing_Shock")
+      {
+        if( player.GetModifiersManager().IsModifierActive(eModifiers.MDF_EPINEPHRINE ) )//effectively resets the timer
+        {
+          player.GetModifiersManager().DeactivateModifier( eModifiers.MDF_EPINEPHRINE );
+        }
+        player.GetModifiersManager().ActivateModifier( eModifiers.MDF_EPINEPHRINE );
+      }
+      else if (item.GetType() == "SRP_BandageDressing_Antibiotic")
+      {
+        if( player.GetModifiersManager().IsModifierActive(eModifiers.MDF_ANTIBIOTICS))//effectively resets the timer
+        {
+          player.GetModifiersManager().DeactivateModifier( eModifiers.MDF_ANTIBIOTICS );
+        }
+        player.GetModifiersManager().ActivateModifier( eModifiers.MDF_ANTIBIOTICS );
+      }
+    }
+    super.ApplyBandage(item, player);
+	}
+};
