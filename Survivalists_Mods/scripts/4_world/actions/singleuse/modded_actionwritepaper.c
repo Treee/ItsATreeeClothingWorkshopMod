9847f0a0642a9238eb74e403f4f23ab076b0a83e
Treee <itsatreee.twitch@gmail.com>
2022-01-11 19:02:52 -0700

    adding the ability to rewrite notes

---
+++ Survivalists_Mods/scripts/4_world/actions/singleuse/modded_actionwritepaper.c
@@ -0,0 +1,12 @@
+modded class ActionWritePaper
+{
+	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+    bool canAction = super.ActionCondition(player, target, item);
+    bool isPenAndPaperCombo1 = (item && item.ConfigIsExisting("writingColor") && target.GetObject() && WrittenNote.Cast(target.GetObject()));
+    bool isPenAndPaperCombo2 = (target && target.GetObject() && target.GetObject().ConfigIsExisting("writingColor") && item && WrittenNote.Cast(item));
+
+		return canAction || isPenAndPaperCombo1 || isPenAndPaperCombo2;
+	}
+
+};
