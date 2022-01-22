cd703dfde1e6966e51015f05d460d0236dcab0c6
Treee <itsatreee.twitch@gmail.com>
2022-01-22 10:48:50 -0700

    adding acid effect ppe requester

--- Survivalists_Mods/scripts/3_game/modded_pperequesterbank.c
+++ Survivalists_Mods/scripts/3_game/modded_pperequesterbank.c
@@ -6,5 +6,6 @@ modded class PPERequesterRegistrations extends Managed
 		PPERequesterBank.RegisterRequester(PPERequester_SRPTobaccoEffect);
 		PPERequesterBank.RegisterRequester(PPERequester_SRPMethEffect);
 		PPERequesterBank.RegisterRequester(PPERequester_SRPBathSaltEffect);
+		// PPERequesterBank.RegisterRequester(PPERequester_SRPAcidEffect);
 	}
 };
