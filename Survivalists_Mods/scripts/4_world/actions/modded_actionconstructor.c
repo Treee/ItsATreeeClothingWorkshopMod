5e4d2a726ef65c3d64e0789697319824c23c02d6
Treee <itsatreee.twitch@gmail.com>
2022-02-13 16:57:32 -0700

    removing face paint action from main mod

--- Survivalists_Mods/scripts/4_world/actions/modded_actionconstructor.c
+++ Survivalists_Mods/scripts/4_world/actions/modded_actionconstructor.c
@@ -7,8 +7,6 @@ modded class ActionConstructor
 		actions.Insert(ActionToggleMuteTransmitter);  
 		actions.Insert(ActionInteractWithGUICraftingWorkbench);
 		actions.Insert(SRP_ActionOpenMap);
-    actions.Insert(ActionPaintFace);
-    actions.Insert(ActionFacePaintStickSwitch);
     actions.Insert(ActionTeleportToEvent);
     actions.Insert(ActionTeleportAwayFromEvent);
 
