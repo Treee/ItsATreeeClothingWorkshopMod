dbf475d0dd44f0d1fa2ea6fa6b692c67a33adc3c
Treee <itsatreee.twitch@gmail.com>
2022-05-23 18:35:42 -0600

    reducing pot fire damage

--- Survivalists_Mods/scripts/3_game/srp_gameconstants.c
+++ Survivalists_Mods/scripts/3_game/srp_gameconstants.c
@@ -31,7 +31,9 @@ modded class GameConstants
 	const int REFRESHER_MAX_DURATION_DEFAULT		= 3600 * 24 * 10; 	//max duration of refresher in seconds - 40 days (+ 5 days final refresh )
 	const int REFRESHER_FREQUENCY_DEFAULT 			= 3600 * 24 * 5;	//frequency of lifetime refreshes/refresher time decreases - 5 days 
 	const float REFRESHER_RADIUS 					= 80; 				//meters
-	
+		
+  const float FIRE_ATTACHMENT_DAMAGE_PER_SECOND 		= 0.016;		// default 0.125
+
 }
 
 modded class EmoteConstants
