5b2664c1aff980cf981611eba04ab710742a5abd
Treee <itsatreee.twitch@gmail.com>
2022-07-19 10:26:44 -0600

    fixing infinite blood bottles

--- Survivalists_Mods/scripts/4_world/entities/itembase/drinks/srp_waterbottlebase.c
+++ Survivalists_Mods/scripts/4_world/entities/itembase/drinks/srp_waterbottlebase.c
@@ -1,1 +1,11 @@
 class SRP_WaterBottleBase extends Bottle_Base {};
+
+
+class SRP_Blood_Bottle extends SRP_WaterBottleBase
+{
+  override void SetActions()
+	{
+		super.SetActions();
+		RemoveAction(ActionFillBottleBase);
+	}
+};
