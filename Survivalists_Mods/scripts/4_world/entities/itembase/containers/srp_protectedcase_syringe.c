ed9cd9f2a028306805f4489950a39298e028a8b6
Treee <itsatreee.twitch@gmail.com>
2022-09-05 17:14:51 -0600

    fixing syringe protector case

--- Survivalists_Mods/scripts/4_world/entities/itembase/containers/srp_protectedcase_syringe.c
+++ Survivalists_Mods/scripts/4_world/entities/itembase/containers/srp_protectedcase_syringe.c
@@ -2,31 +2,23 @@ class SRP_SmallProtectorCase_Syringe extends SmallProtectorCase
 {
   override bool CanReceiveItemIntoCargo (EntityAI item)
 	{
-		return item.IsKindOf("ClearSyringe") || item.IsKindOf("Syringe") || item.IsKindOf("BloodSyringe");
+    return item.GetType().Contains("DUB_Mutantsyringe");
 	}
 
   override bool CanLoadItemIntoCargo( EntityAI item )
   {
-		return item.IsKindOf("ClearSyringe") || item.IsKindOf("Syringe") || item.IsKindOf("BloodSyringe");
+    return item.GetType().Contains("DUB_Mutantsyringe");
   }
 };
 class SRP_SmallProtectorCase_FlareBox extends SmallProtectorCase
 {
   override bool CanReceiveItemIntoCargo (EntityAI item)
 	{
-    if (item.GetType().Contains("Ammo_Flare"))
-    {
-      return true;
-    }
-    return false;
+    return item.GetType().Contains("Ammo_Flare");
 	}
 
   override bool CanLoadItemIntoCargo( EntityAI item )
   {
-		if (item.GetType().Contains("Ammo_Flare"))
-    {
-      return true;
-    }
-    return false;
+    return item.GetType().Contains("Ammo_Flare");
   }
 };
