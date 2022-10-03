daf7b34d24f638ef7e966401849cbfd6e30a0c4b
Treee <itsatreee.twitch@gmail.com>
2022-10-03 12:41:32 -0600

    fixing combat logger for 1.19

--- CombatLogger/scripts/5_mission/mission/modded_missionserver.c
+++ CombatLogger/scripts/5_mission/mission/modded_missionserver.c
@@ -2,9 +2,9 @@ modded class MissionServer
 {
   override void InvokeOnDisconnect( PlayerBase player )
   {
-    if (player)
+    if (player && player.m_AdminLog)
     {
-      player.m_AdminLog.LogPrint(string.Format("%1 (%2) Logged out at %3", player.GetIdentity().GetPlainId(), player.GetIdentity().GetName(), player.GetPosition()));
+      player.m_AdminLog.LogPrint(string.Format("[COMBAT LOGGER] - SteamdID: (%1) ID: (%2) Name: (%3) Logged out at (%4)", player.GetIdentity().GetPlainId(),player.GetIdentity().GetId(), player.GetIdentity().GetName(), player.GetPosition()));
     }
     super.InvokeOnDisconnect(player);
   }
