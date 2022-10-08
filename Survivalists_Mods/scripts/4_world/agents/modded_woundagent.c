bbd9fbde04ad4270a5bf9b8fd1bde4a3524e25a0
Treee <itsatreee.twitch@gmail.com>
2022-10-08 10:57:02 -0600

    reducing wound agent invasibility

--- Survivalists_Mods/scripts/4_world/agents/modded_woundagent.c
+++ Survivalists_Mods/scripts/4_world/agents/modded_woundagent.c
@@ -5,7 +5,7 @@ modded class WoundAgent
 	override void Init()
 	{
     super.Init();
-		m_Invasibility 			= 0.052;//to reach 250 in 60 mins
+		m_Invasibility 			= 0.022;//to reach 250 in 60 mins
 		m_MaxCount 				= 1000; // 4 hours to get max infection
 	}
 }
