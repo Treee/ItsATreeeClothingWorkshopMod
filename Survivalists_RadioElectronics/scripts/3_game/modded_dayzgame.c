cb9222f97b05056f6378ccaef216002f92587cca
Treee <itsatreee.twitch@gmail.com>
2022-12-05 15:00:52 -0700

    adding back in array creation that was removed accidentally

--- Survivalists_RadioElectronics/scripts/3_game/modded_dayzgame.c
+++ Survivalists_RadioElectronics/scripts/3_game/modded_dayzgame.c
@@ -30,32 +30,31 @@ modded class DayZGame
       if (!FileExist("$profile:\\Survivalists_RadioElectronics\\RadioElectronicsConfig.json")) 
       {
         m_RadioElectronicsConfig = new RadioElectronicsConfig();
-
+        
         m_RadioElectronicsConfig.m_RadioTowerInfo = new ref array<ref SRPRadioTowerInfo>;
-
         // Land_Tower_TC1
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("2154 35 14615",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("1711 46 12767",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3745 55 2862",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3750 63 4887",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3781 72 3735",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5187 89 3575",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5357 81 1499",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5543 202 6789",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7340 80 6033",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7515 107 4640",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7821 133 1971",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("9534 117 3720",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("10027 87 4781",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("2154 45 14615",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("1711 55 12767",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3745 60 2862",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3750 71 4887",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3781 80 3735",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5187 97 3575",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5357 90 1499",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5543 210 6789",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7340 90 6033",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7515 115 4640",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7821 140 1971",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("9534 131 3720",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("10027 100 4781",15));
         m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("11091 75 11009",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("11529 96 8127",15));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("11854 109 2720",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("11529 110 8127",15));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("11854 120 2720",15));
         // Land_Tower_TC2_Base
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5820 113 4096",10));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7395 726 11939",10));
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("1021 48 15242",10));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("5820 116 4096",10));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("7395 729 11939",10));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("1021 51 15242",10));
         // Land_Tower_TC5
-        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("9334 53 8823",10));
+        m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("9334 56 8823",10));
         // Land_Tower_TC6
         m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3319 68 3819",10));
         m_RadioElectronicsConfig.m_RadioTowerInfo.Insert(new SRPRadioTowerInfo("3745 55 6202",10));
