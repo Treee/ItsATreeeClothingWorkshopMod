08c8d69f68952cee2e8cea49dca063945173e5a0
Treee <itsatreee.twitch@gmail.com>
2022-12-05 10:45:07 -0700

    fixing mutant spawning issue, removing duplicate old winter parka retextures. added recipes for a few items

--- Survivalists_Mods/scripts/3_game/srp_moddedgameplay.c
+++ Survivalists_Mods/scripts/3_game/srp_moddedgameplay.c
@@ -11,3 +11,16 @@ modded class Hud
 {
   void DisplayTirednessNotifier( int key, int currentTirednessCount, int tirednessDelta, int status );
 };
+
+modded class MenuDefaultCharacterData
+{
+  override string GetCharacterType()
+	{
+    if (m_CharacterType == "DUB_SurMutant_M" || m_CharacterType == "DUB_SurMutant_M_2")
+      return "SurvivorM_Mirek";
+    else if (m_CharacterType == "DUB_SurMutant_F" || m_CharacterType == "DUB_SurMutant_F_2")
+      return "SurvivorF_Eva";
+		else 
+      return m_CharacterType;
+	}
+};
