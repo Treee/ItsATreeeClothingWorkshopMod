42f04ed576349f9b91f63170b6d87410331de033
Treee <itsatreee.twitch@gmail.com>
2022-12-21 17:40:26 -0700

    standardize how much dye it takes for clothing categories

--- Survivalists_Mods/scripts/4_world/entities/itembase/consumables/srp_dyes.c
+++ Survivalists_Mods/scripts/4_world/entities/itembase/consumables/srp_dyes.c
@@ -6,6 +6,39 @@ class SRP_CraftingMaterial_Dye_Base extends ItemBase
 		
 		AddAction(ActionSRPRecolorClothingOption);
 	}
+
+  void ReduceQuantityBySlotType(int slottype)
+  {
+    // -1 is no slot type so short circuit
+    if (slottype == -1)
+      return;
+
+    int quantityReduced = 0;
+    if (slottype == InventorySlots.HEADGEAR)
+      quantityReduced = 15;
+    else if (slottype == InventorySlots.MASK)
+      quantityReduced = 10;
+    else if (slottype == InventorySlots.EYEWEAR)
+      quantityReduced = 5;
+    else if (slottype == InventorySlots.GLOVES)
+      quantityReduced = 15;
+    else if (slottype == InventorySlots.ARMBAND)
+      quantityReduced = 10;
+    else if (slottype == InventorySlots.BODY)
+      quantityReduced = 35;
+    else if (slottype == InventorySlots.VEST)
+      quantityReduced = 30;
+    else if (slottype == InventorySlots.BACK)
+      quantityReduced = 50;
+    else if (slottype == InventorySlots.HIPS)
+      quantityReduced = 20;
+    else if (slottype == InventorySlots.LEGS)
+      quantityReduced = 35;
+    else if (slottype == InventorySlots.FEET)
+      quantityReduced = 15;
+
+    AddQuantity(-quantityReduced);
+  }
 };
 
 class SRP_CraftingMaterial_Dye_Bleach extends SRP_CraftingMaterial_Dye_Base{};
