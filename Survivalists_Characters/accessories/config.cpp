ec44f84e6a3440e9bdf0133f2c2427727eb62cf5
Treee <itsatreee.twitch@gmail.com>
2022-12-21 19:45:01 -0700

    adding 2 rings

--- Survivalists_Characters/accessories/config.cpp
+++ Survivalists_Characters/accessories/config.cpp
@@ -25,7 +25,7 @@ class CfgVehicles
 		displayName="Wedding Ring";
 		descriptionShort="A silver wedding ring";
 		model="Survivalists_Characters\accessories\Wedding_Ring_g.p3d";
-		inventorySlot="Armband";
+		inventorySlot="Rings";
 		simulation="clothing";
 		vehicleClass="Clothing";
 		itemInfo[]=
@@ -67,6 +67,131 @@ class CfgVehicles
 					};
 				};
 			};
+		};
+	};
+  class SRP_Ring_Earth_ColorBase: Clothing
+	{
+		scope=0;
+		displayName="Earthen Ring";
+		descriptionShort="A ring of earthen color.";
+		model="Survivalists_Characters\accessories\srp_ring_earth_g.p3d";
+		inventorySlot="Rings";
+		simulation="clothing";
+		vehicleClass="Clothing";
+		itemInfo[]=
+		{
+			"Clothing",
+			"Armband"
+		};
+		weight=1;
+		itemSize[]={1,1};
+		absorbency=0;
+		heatIsolation=0;
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"Survivalists_Characters\accessories\data\srp_ring_earth_co.paa"
+		};
+		class ClothingTypes
+		{
+			male="Survivalists_Characters\accessories\srp_ring_earth_m.p3d";
+			female="Survivalists_Characters\accessories\srp_ring_earth_f.p3d";
+		};
+    class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+					healthLevels[]=
+					{
+						{1.0,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.69999999,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.5,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.30000001,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.0,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}}
+					};
+				};
+			};
+		};
+	};
+  class SRP_Ring_Earth_Basic: SRP_Ring_Earth_ColorBase
+	{
+		scope=2;
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"Survivalists_Characters\accessories\data\srp_ring_earth_co.paa"
+		};
+	};
+
+  class SRP_Ring_Celtic_ColorBase: Clothing
+	{
+		scope=0;
+		displayName="Celtic Ring";
+		descriptionShort="A ring of celtic theme.";
+		model="Survivalists_Characters\accessories\srp_ring_celtic_g.p3d";
+		inventorySlot="Rings";
+		simulation="clothing";
+		vehicleClass="Clothing";
+		itemInfo[]=
+		{
+			"Clothing",
+			"Armband"
+		};
+		weight=1;
+		itemSize[]={1,1};
+		absorbency=0;
+		heatIsolation=0;
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"Survivalists_Characters\accessories\data\srp_ring_celtic_co.paa"
+		};
+		class ClothingTypes
+		{
+			male="Survivalists_Characters\accessories\srp_ring_celtic_m.p3d";
+			female="Survivalists_Characters\accessories\srp_ring_celtic_f.p3d";
+		};
+    class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+					healthLevels[]=
+					{
+						{1.0,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.69999999,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.5,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.30000001,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}},
+            {0.0,{"Survivalists_Characters\accessories\data\srp_ring_earth.rvmat"}}
+					};
+				};
+			};
+		};
+	};
+  class SRP_Ring_Celtic_Basic: SRP_Ring_Celtic_ColorBase
+	{
+		scope=2;
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"Survivalists_Characters\accessories\data\srp_ring_celtic_co.paa"
 		};
 	};
 
--- Survivalists_Mods/README.md
+++ Survivalists_Mods/README.md
@@ -920,6 +920,12 @@ You wanted to run forever so now you can. Don't stop your you will drop!
 
 - "Sci Fi Mobile Teleporter" (https://skfb.ly/osTC8) by Sousinho is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
+- "Ring Of Earth" (https://skfb.ly/6VpIs) by AOL_DarkHairLion is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+- "Celtic silver ring" (https://skfb.ly/6YC6Z) by Aren Boada is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+- "Sword Barbarian" (https://skfb.ly/oxpO9) by AzTiZ is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
 - https://soundcloud.com/benjamin-w-musi/lepa-brena-hej-seki-seki
 
 - https://soundcloud.com/walkintree/kalasnjikov-by-goran-bregovic
