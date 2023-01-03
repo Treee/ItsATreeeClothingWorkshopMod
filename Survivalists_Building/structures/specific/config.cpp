a9d860df1f189a681a7343bd56be506206930c14
Treee <itsatreee.twitch@gmail.com>
2023-01-03 16:08:41 -0700

    adding pile of gold coins

---
+++ Survivalists_Building/structures/specific/config.cpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+	class Survivalists_Building_Structures_Specific
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+      "DZ_Structures_Specific",
+      "Survivalists_Building_Gear_Camping"
+		};
+	};
+};
+class CfgVehicles
+{
+  class HouseNoDestruct;
+  class Land_Building_Specific_PileOfGold: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\srp_treasurepile_gold_co.paa"};
+		model="Survivalists_Building\structures\specific\srp_treasurepile_gold.p3d";
+	};
+};
