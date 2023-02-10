2d7cc1704a70c70bc80c43eb8302337cc711cca8
Treee <itsatreee.twitch@gmail.com>
2023-02-10 12:53:23 -0700

    moving coupled code into correct spots

--- Survivalists_Scripts/scripts/4_world/classes/recipes/modded_pluginrecipesmanager.c
+++ Survivalists_Scripts/scripts/4_world/classes/recipes/modded_pluginrecipesmanager.c
@@ -592,6 +592,34 @@ modded class PluginRecipesManager
 
       RegisterRecipe(new CraftHotdog);
 
+
+    //==========================================================SMELTING       
+      RegisterRecipe(new Craft_SRP_Container_StonePileSmall);
+      RegisterRecipe(new Craft_SRP_Container_StonePileLarge);
+
+      RegisterRecipe(new Craft_SRP_CraftingMaterial_Mortar_Crude);
+      RegisterRecipe(new Craft_SRP_CraftingMaterial_Cement_Crude);
+
+      RegisterRecipe(new Craft_SRP_StoneForgeWorkbenchStarter_Kit);
+      RegisterRecipe(new Craft_SRP_StoneForgeWorkbenchFinisher_Kit);
+      RegisterRecipe(new Craft_SRP_StoneForgeWorkbench_Kit);
+
+      RegisterRecipe(new Craft_SRP_MetalCoins);
+      RegisterRecipe(new Craft_SRP_ForgeIngotPlate_ColorBase);
+      RegisterRecipe(new Craft_SRP_ForgeIngotRod_ColorBase);
+
+      RegisterRecipe(new Craft_SRP_CrushLargeStone_CrudeTool);
+      RegisterRecipe(new Craft_SRP_CrushStoneChunk_CrudeTool);
+      RegisterRecipe(new Craft_SRP_CrushStoneChunk_JewlingTool);
+      RegisterRecipe(new Craft_SRP_RefineUnCutGem_CrudeTool);
+      RegisterRecipe(new Craft_SRP_RefineUnCutGem_JewlCuttingTool);
+      RegisterRecipe(new Craft_SRP_CrucibleEmptyLime);
+      RegisterRecipe(new Craft_SRP_CrucibleEmptyMortar);
+      RegisterRecipe(new Craft_SRP_MetalBucket);
+      RegisterRecipe(new Craft_SRP_IngotMoldEmptyMetal);
+      RegisterRecipe(new Craft_SRP_IngotMold_Lime);
+      RegisterRecipe(new Craft_SRP_IngotMold_Mortar);
+
     //==========================================================Gunsmithing           
       RegisterRecipe(new SRP_BreakDownPistolCore_Expert);
       RegisterRecipe(new SRP_BreakDownRifleCoreLarge_Expert);
