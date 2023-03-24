da5f6ab7a929e53b33fe505240c4c31017b9c37f
Treee <itsatreee.twitch@gmail.com>
2023-03-24 13:37:46 -0600

    fixing issue where rags in hand were cuasing other actions to be hidden

--- Survivalists_FacePaints/scripts/4_world/srp_actionwashface.c
+++ Survivalists_FacePaints/scripts/4_world/srp_actionwashface.c
@@ -16,6 +16,7 @@ class ActionWashFace: ActionContinuousBase
 		m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_SHAVE;
 		m_FullBody = false;
 		m_StanceMask = DayZPlayerConstants.STANCEMASK_ERECT | DayZPlayerConstants.STANCEMASK_CROUCH;
+    m_Text = "Wash Face";
 	}
 	
 	override void CreateConditionComponents()  
@@ -26,34 +27,16 @@ class ActionWashFace: ActionContinuousBase
 
   override bool ActionCondition ( PlayerBase player, ActionTarget target, ItemBase item )
 	{	
-    PlayerBase man;
-    if (target && Class.CastTo(man, target.GetObject()) )
+    if (player && player.GetFacePaintIndex() > -1)
     {
-      return false;
+      return true;
     }
-    if (player && player.GetFacePaintIndex() == -1)
-    {
-      return false;
-    }
-    if (player && player.IsBleeding())
-    {
-      return false;
-    }
-
-    int slot_id = InventorySlots.GetSlotIdFromString("Mask");	
-		EntityAI equipedMask = player.GetInventory().FindPlaceholderForSlot( slot_id );
-
-    slot_id = InventorySlots.GetSlotIdFromString("Eyewear");	
-		EntityAI equipedGlasses = player.GetInventory().FindPlaceholderForSlot( slot_id );
-    // cannot apply when wearing eyewear or masks
-    return !(equipedMask || equipedGlasses);
+    return false;
 	}
-
-  override string GetText()
+	override bool HasTarget()
 	{
-		return "Wash Face";
+		return false;
 	}
-
   override void OnFinishProgressServer( ActionData action_data )
 	{	
     if (action_data.m_Player)
