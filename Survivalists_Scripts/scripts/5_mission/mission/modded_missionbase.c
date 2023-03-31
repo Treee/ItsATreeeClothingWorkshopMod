2105fcffef2c56941e29c77372147124806f2312
Treee <itsatreee.twitch@gmail.com>
2023-03-31 09:49:45 -0600

    removing errors

--- Survivalists_Scripts/scripts/5_mission/mission/modded_missionbase.c
+++ Survivalists_Scripts/scripts/5_mission/mission/modded_missionbase.c
@@ -1,16 +1,16 @@
-modded class MissionBase
-{
-  override UIScriptedMenu CreateScriptedMenu(int id)
-	{
-    UIScriptedMenu menu = NULL;
-		if (id == SRP_MENU_RADIAL_MULTI)
-      menu = new SRP_RadialMenuDisplay_MultiCategory;
+// modded class MissionBase
+// {
+//   override UIScriptedMenu CreateScriptedMenu(int id)
+// 	{
+//     UIScriptedMenu menu = NULL;
+// 		if (id == SRP_MENU_RADIAL_MULTI)
+//       menu = new SRP_RadialMenuDisplay_MultiCategory;
 
-    if (menu)
-    {
-			menu.SetID(id);
-      return menu;
-    }
-    return super.CreateScriptedMenu(id);
-  }
-};
+//     if (menu)
+//     {
+// 			menu.SetID(id);
+//       return menu;
+//     }
+//     return super.CreateScriptedMenu(id);
+//   }
+// };
