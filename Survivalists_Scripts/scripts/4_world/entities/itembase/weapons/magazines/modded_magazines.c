da14c732a6ec03dd8d67b7965109b79c76003109
Treee <itsatreee.twitch@gmail.com>
2023-04-05 14:21:32 -0600

    fixing overide problem

--- Survivalists_Scripts/scripts/4_world/entities/itembase/weapons/magazines/modded_magazines.c
+++ Survivalists_Scripts/scripts/4_world/entities/itembase/weapons/magazines/modded_magazines.c
@@ -73,182 +73,182 @@
 //================================================ FLAT RIFLE MAGAZINES
   modded class Mag_AK74_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_AK74_45Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_AK101_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_AKM_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_AKM_Palm30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_Aug_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_STANAG_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_CMAG_10Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_CMAG_20Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_CMAG_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_CZ61_20Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_CZ527_5rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_FAL_20Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_FAMAS_25Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_MP5_15Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_MP5_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_Groza_20Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_VSS_10Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_PM73_25Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_PM73_15Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_Ruger1022_30Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_Ruger1022_15Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_Scout_5Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_SVD_10Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_UMP_25Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
   };
   modded class Mag_VAL_20Rnd
   {
-    bool IsFlatRifleMagazine()
+    override bool IsFlatRifleMagazine()
     {
       return true;
     }
