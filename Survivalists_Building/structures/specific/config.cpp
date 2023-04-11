08361389540a9edb6ea0d64017dbcfbcdefdb2b9
Treee <itsatreee.twitch@gmail.com>
2023-04-11 17:17:46 -0600

    adding static items for paper hints

--- Survivalists_Building/structures/specific/config.cpp
+++ Survivalists_Building/structures/specific/config.cpp
@@ -28,4 +28,47 @@ class CfgVehicles
     hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\srp_bearstatue_tyler_co.paa"};
 		model="Survivalists_Building\structures\specific\srp_bearstatue.p3d";
 	};
+
+  class Land_Building_Specific_Hint_MiningLocations: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_mining_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
+  class Land_Building_Specific_Hint_FishingLocations: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_fishing_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
+  class Land_Building_Specific_Hint_BrewingStuff: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_brewing_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
+  class Land_Building_Specific_Hint_CDCForm1: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_cdcforminfo_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
+  class Land_Building_Specific_Hint_CDCForm2: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_cdcformsurgury_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
+  class Land_Building_Specific_Hint_CDCForm3: HouseNoDestruct
+	{
+		scope=1;
+    hiddenSelections[]={"zbytek"};
+    hiddenSelectionsTextures[]={"Survivalists_Building\structures\specific\data\loot_paper_cdcformrecord_co.paa"};
+		model="\dz\gear\consumables\Paper.p3d";
+	};
 };
