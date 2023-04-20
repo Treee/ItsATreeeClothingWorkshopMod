cb2c67cf0ffcaa67879a292ec2e74c9bab961bd2
Treee <itsatreee.twitch@gmail.com>
2023-04-20 17:13:47 -0600

    added palisade tower and ramps and platforms

--- Survivalists_Building/structures/fortification/config.cpp
+++ Survivalists_Building/structures/fortification/config.cpp
@@ -115,5 +115,116 @@ class CfgVehicles
 		model="Survivalists_Building\structures\fortification\srp_bamboos_cross.p3d";
     hiddenSelections[]={"zbytek"};
     hiddenSelectionsTextures[]={"Survivalists_Building\structures\fortification\data\srp_barricade_bamboo_yellow_co.paa"};
-	};   
+	};
+//======================================= PALISADE
+  class SRP_Fortifications_PalisadeAll_Kit: SRP_Building_KitBase
+	{
+		scope=0;
+		displayName="Palisade wall All";
+		descriptionShort="Palisade wall All";
+    projectionTypename="SRP_Fortifications_PalisadeAll";
+	};
+  class SRP_Fortifications_PalisadeAll: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_all.p3d";
+	};
+
+  class Land_SRP_Fortifications_PalisadeTower_Small_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Small";
+		descriptionShort="A kit for building a palisade tower.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTowersAll";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Small: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_tower_small.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_LargeStairs_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Large (With Stairs)";
+		descriptionShort="A kit for building a palisade tower.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTowersAll";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_LargeStairs: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_tower_large.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_LargeLadder_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Large (With Ladder)";
+		descriptionShort="A kit for building a palisade tower.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTowersAll";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_LargeLadder: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_tower_largeladder.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Ramp_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Ramp";
+		descriptionShort="A kit for building a ramp.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTower_Ramp";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Ramp: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_ramp.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Stairs_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Kit - Stairs";
+		descriptionShort="A kit for building a Stairs.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTower_Stairs";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Stairs: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_tower_stairs.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Platform_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Kit - Platform";
+		descriptionShort="A kit for building a single platform.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTower_Platform";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_Platform: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_platform.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_PlatformDouble_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Platform Double";
+		descriptionShort="A kit for building a double length platform.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTower_PlatformDouble";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_PlatformDouble: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_platformdouble.p3d";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_PlatformTriple_Kit: SRP_Building_KitBase
+	{
+		scope=2;
+		displayName="Palisade Tower Kit - Platform Triple";
+		descriptionShort="A kit for building a triple platform.";
+    projectionTypename="Land_SRP_Fortifications_PalisadeTower_PlatformTriple";
+	};
+  class Land_SRP_Fortifications_PalisadeTower_PlatformTriple: SRP_PrefabFortification_Base
+	{
+		scope=2;
+		model="Survivalists_Building\structures\fortification\srp_palisade_platformtriple.p3d";
+	};
+
 };
