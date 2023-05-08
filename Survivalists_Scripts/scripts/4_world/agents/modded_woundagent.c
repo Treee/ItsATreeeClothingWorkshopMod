c9266d35953cdb7fb5ba3e1f9fb0bdf97d0cfd9c
Treee <itsatreee.twitch@gmail.com>
2023-05-08 12:14:23 -0600

    making cholera, wound agent and influenza more antibiotic resistant

--- Survivalists_Scripts/scripts/4_world/agents/modded_woundagent.c
+++ Survivalists_Scripts/scripts/4_world/agents/modded_woundagent.c
@@ -1,11 +1,33 @@
 modded class WoundAgent
 {	
-	static const float RESISTANCE_STAGE_2 = 0.65;
+  static const float RESISTANCE_STAGE_1 = 0.5;
+	static const float RESISTANCE_STAGE_2 = 0.85;
 
 	override void Init()
 	{
     super.Init();
-		m_Invasibility 			= 0.022;//to reach 250 in 60 mins
-		m_MaxCount 				= 1000; // 4 hours to get max infection
+		m_Invasibility 			= 0.012;//to reach 250 in 60 mins
+		m_MaxCount 				= 5000; // 4 hours to get max infection
 	}
-}
+};
+
+modded class CholeraAgent
+{
+	override void Init()
+	{
+    super.Init();
+		m_AntibioticsResistance = 0.5;
+		m_MaxCount 				= 5000;
+		m_Potency 				= EStatLevels.GREAT;
+	}
+};
+
+modded class InfluenzaAgent
+{
+	override void Init()
+	{		
+		m_AntibioticsResistance = 0.5;
+		m_MaxCount 				= 3000;
+    m_Potency 				= EStatLevels.GREAT;
+	}
+};
--- Survivalists_Scripts/scripts/4_world/modifiers/modded_woundinfectionmdfr.c
+++ Survivalists_Scripts/scripts/4_world/modifiers/modded_woundinfectionmdfr.c
@@ -1,26 +1,25 @@
 modded class WoundInfectionMdfr
 {
-	static const int AGENT_THRESHOLD_ACTIVATE = 200;
+	static const int AGENT_THRESHOLD_ACTIVATE = 400;
 	static const int AGENT_THRESHOLD_DEACTIVATE = 20;
 
 };
 
 modded class WoundInfectStage1Mdfr
 {
-	static const int AGENT_THRESHOLD_DEACTIVATE = 500;
-	static const int AGENT_THRESHOLD_FEVER = 500;
+	static const int AGENT_THRESHOLD_DEACTIVATE = 1250;
+	static const int AGENT_THRESHOLD_FEVER = 900;
 	
-	static const int PAIN_EVENT_INTERVAL_MIN = 120;
-	static const int PAIN_EVENT_INTERVAL_MAX = 300;
+	static const int PAIN_EVENT_INTERVAL_MIN = 300;
+	static const int PAIN_EVENT_INTERVAL_MAX = 900;
 };
 
 modded class WoundInfectStage2Mdfr
 {
-	static const int AGENT_THRESHOLD_ACTIVATE = 500;
+	static const int AGENT_THRESHOLD_ACTIVATE = 3000;
 
-	static const int PAIN_EVENT_INTERVAL_MIN = 60;
-	static const int PAIN_EVENT_INTERVAL_MAX = 180;	
+	static const int PAIN_EVENT_INTERVAL_MIN = 300;
+	static const int PAIN_EVENT_INTERVAL_MAX = 900;	
 	
-	static const float DAMAGE_PER_SEC = 0.028;
-
+	static const float DAMAGE_PER_SEC = 0.018;
 };
