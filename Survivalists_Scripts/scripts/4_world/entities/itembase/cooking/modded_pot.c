1cbb8474a3a71a4abbc393069a1ad81e4a4c6dcc
Treee <itsatreee.twitch@gmail.com>
2023-05-08 22:22:36 -0600

    fixing infinite storage bugs

--- Survivalists_Scripts/scripts/4_world/entities/itembase/cooking/modded_pot.c
+++ Survivalists_Scripts/scripts/4_world/entities/itembase/cooking/modded_pot.c
@@ -2,11 +2,19 @@ modded class Pot
 {
   override bool CanPutInCargo( EntityAI parent )
 	{
-		if (GetQuantity() > 100 && parent.GetHierarchyRootPlayer())
-    { // if you have more than 100ml of liquid and we are trying to put into an item attached to a player
+    // if you have more than 100ml of liquid and we are trying to put into an item attached to a player
+		if (GetQuantity() > 100 && parent.GetHierarchyRootPlayer())    
       return false; // dont attach
-    }
 		return super.CanPutInCargo(parent);
+	}
+
+  override bool CanReceiveItemIntoCargo(EntityAI item)
+	{
+		if (!super.CanReceiveItemIntoCargo(item))
+      return false;
+		if (item.IsContainer())
+      return false;
+    return true;
 	}
 };
 
@@ -17,20 +25,14 @@ modded class PortableGasStove
     if (GetCookingEquipment())
     { // if you have more than 100ml of liquid and we are trying to put into an item attached to a player
       if (GetCookingEquipment().GetQuantity() > 100 && parent.GetHierarchyRootPlayer())
-      {
         return false;
-      }
       // if you have items on the pot and you try to store the gas stove, (small cargo exploit)
-      if (GetCookingEquipment().GetInventory().CountInventory() > 0)
-      {
-        return false;
-      }
+      if (GetCookingEquipment().GetInventory().CountInventory() == 0)
+        return true;
     }
     ItemBase gasTank;
     if (Class.CastTo(gasTank, GetItemOnSlot("GasCanister")))
-    {
       return false;
-    }    
     return super.CanPutInCargo(parent);
 	}
 };
@@ -45,5 +47,17 @@ modded class PortableGasLamp
       return false;
     }    
     return super.CanPutInCargo(parent);
+	}
+};
+
+modded class Bear_ColorBase
+{
+  override bool CanReceiveItemIntoCargo(EntityAI item)
+	{
+		if (!super.CanReceiveItemIntoCargo(item))
+      return false;
+		if (item.IsContainer())
+      return false;
+    return true;
 	}
 };
