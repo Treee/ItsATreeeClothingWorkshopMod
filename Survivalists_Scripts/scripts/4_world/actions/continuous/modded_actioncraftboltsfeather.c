7f2c0f05358d6695dde047d07c65573cc0842db7
Treee <itsatreee.twitch@gmail.com>
2023-05-15 10:31:30 -0600

    increasing the amount of feathers needed to craft bolts

---
+++ Survivalists_Scripts/scripts/4_world/actions/continuous/modded_actioncraftboltsfeather.c
@@ -0,0 +1,42 @@
+modded class ActionCraftBoltsFeather
+{
+	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+		if (IsFeatherType(item.ClassName()))
+		{
+      if (item.GetQuantity() < 5)
+        return false;
+			//feather in hands
+			if (target.GetObject())
+				return (target.GetObject().ClassName() == "Ammo_ImprovisedBolt_1");
+		}
+		else if (target.GetObject())
+		{
+      ItemBase feather;
+      if (Class.CastTo(feather, target.GetObject()))
+        if (feather.GetQuantity() < 5)
+          return false;
+
+			// bolt in hands
+			return IsFeatherType(target.GetObject().ClassName());
+		}
+		return false;
+	}
+
+	override void OnFinishProgressServer(ActionData action_data)
+	{
+    super.OnFinishProgressServer(action_data);
+    ItemBase feather;
+		if (m_IsFeatherInHands)
+		{
+			//feather in hands
+			feather = action_data.m_MainItem;
+		  feather.AddQuantity(-4);	// 1 minus total since base game already removes 1		
+		}
+    else
+    {
+      if (Class.CastTo(feather, action_data.m_Target.GetObject()))
+        feather.AddQuantity(-4);
+    }
+	}
+};
