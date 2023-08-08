d83fbc87d9fdb249802fd9bb6ba4d487977121e4
Treee <itsatreee.twitch@gmail.com>
2023-08-08 09:07:39 -0600

    balance out kuru a bit more

--- Survivalists_Scripts/scripts/4_world/modifiers/modded_braindiseasemdfr.c
+++ Survivalists_Scripts/scripts/4_world/modifiers/modded_braindiseasemdfr.c
@@ -1,16 +1,16 @@
-modded class BrainDiseaseMdfr: ModifierBase
+modded class BrainDiseaseMdfr
 {
 	static const int AGENT_THRESHOLD_ACTIVATE = 7500;
-	const int SHAKE_INTERVAL_MIN = 120;
-	const int SHAKE_INTERVAL_MAX = 300;
+	const int SHAKE_INTERVAL_MIN = 3600;
+	const int SHAKE_INTERVAL_MAX = 43200;
 
 	override protected void OnTick(PlayerBase player, float deltaT)
 	{
 		m_Time += deltaT;
-		float brain_agents = player.GetSingleAgentCountNormalized(eAgents.BRAIN) / 8.0;
 		
 		if( m_Time >= m_ShakeTime )
 		{
+		  float brain_agents = player.GetSingleAgentCountNormalized(eAgents.BRAIN) / 8.0;
 			DayZPlayerSyncJunctures.SendKuruRequest(player, brain_agents);
 			m_ShakeTime = m_Time + Math.RandomFloat(SHAKE_INTERVAL_MIN, SHAKE_INTERVAL_MAX);
       m_Time = 0;
