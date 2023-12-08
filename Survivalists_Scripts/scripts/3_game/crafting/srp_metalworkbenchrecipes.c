a9ac064bd659d1c8982b54379b5bd43cbf51140d
Treee <itsatreee.twitch@gmail.com>
2023-12-08 15:55:01 -0700

    adding tire rack recipe

--- Survivalists_Scripts/scripts/3_game/crafting/srp_metalworkbenchrecipes.c
+++ Survivalists_Scripts/scripts/3_game/crafting/srp_metalworkbenchrecipes.c
@@ -15,7 +15,8 @@ class SRP_MetalWorkbenchRecipes extends SRP_RecipeManager
     craftableItems.Insert(Register_UmbrellaKit_Recipe());
     craftableItems.Insert(Register_TableWithUmbrellaKit_Recipe());
     craftableItems.Insert(Register_FurnitureTeaKettle_Recipe());    
-    craftableItems.Insert(Register_ArmorBucketHelmet_Recipe());        
+    craftableItems.Insert(Register_ArmorBucketHelmet_Recipe());    
+    craftableItems.Insert(Register_TireRack_Recipe());            
   }
 //===========================TEMPLATE
   // SRP_CraftableItem Register_X_Recipe()
@@ -287,6 +288,30 @@ class SRP_MetalWorkbenchRecipes extends SRP_RecipeManager
     craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L2W2_MetalSheets", SRP_COLOR.NONE, 2));
 
     craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_Shelter_Fabric", SRP_COLOR.NONE, 4));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("MetalWire", SRP_COLOR.NONE, 1));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_MetalWire", SRP_COLOR.NONE, 1));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_FPole_MetalWire", SRP_COLOR.NONE, 1));
+    return craftableItem;
+  } 
+  SRP_CraftableItem Register_TireRack_Recipe()
+  {
+    SRP_CraftableItem craftableItem = new SRP_CraftableItem("SRP_TireRack", "Tire Storage Rack");
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("SRP_WoodDrill", SRP_COLOR.NONE, 1));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("SRP_WoodClamp", SRP_COLOR.NONE, 1));
+
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W1_MetalSheets", SRP_COLOR.NONE, 2));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W2_MetalSheets", SRP_COLOR.NONE, 2));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W3_MetalSheets", SRP_COLOR.NONE, 5));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L2W1_MetalSheets", SRP_COLOR.NONE, 2));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L2W2_MetalSheets", SRP_COLOR.NONE, 2));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L2W3_MetalSheets", SRP_COLOR.NONE, 5));
+
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_Nails", SRP_COLOR.NONE, 10));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1_Nails", SRP_COLOR.NONE, 10));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W1_Nails", SRP_COLOR.NONE, 5));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W2_Nails", SRP_COLOR.NONE, 10));
+    craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_L1W3_Nails", SRP_COLOR.NONE, 5));
+
     craftableItem.RegisterIngredient(new SRP_ItemRequirement("MetalWire", SRP_COLOR.NONE, 1));
     craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_MetalWire", SRP_COLOR.NONE, 1));
     craftableItem.RegisterIngredient(new SRP_ItemRequirement("Material_FPole_MetalWire", SRP_COLOR.NONE, 1));
