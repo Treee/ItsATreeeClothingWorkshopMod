0f618d323f21308f0f12cb4ce657473cdb81765e
Treee <itsatreee.twitch@gmail.com>
2024-02-03 18:28:28 -0700

    fixing other smelting crafting recipes for the bucket

--- Survivalists_Scripts/scripts/4_world/classes/recipes/crafting/srp_mining_recipes.c
+++ Survivalists_Scripts/scripts/4_world/classes/recipes/crafting/srp_mining_recipes.c
@@ -579,8 +579,8 @@ class Craft_SRP_CrucibleEmptyLime extends RecipeBase
 		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient
 		
 		//ingredient 2
-		InsertIngredient(1,"SRP_MetalBucket_Default");//you can insert multiple ingredients this way
-		
+		// InsertIngredient(1,"SRP_MetalBucket_Default");//you can insert multiple ingredients this way
+		InsertIngredient(1,"SRP_MetalBucket");//you can insert multiple ingredients this way		
 		m_IngredientAddHealth[1] = 0;// 0 = do nothing
 		m_IngredientSetHealth[1] = -1; // -1 = do nothing
 		m_IngredientAddQuantity[1] = 0;// 0 = do nothing
@@ -775,7 +775,8 @@ class Craft_SRP_IngotMoldEmptyMetal extends RecipeBase
 		
 		//INGREDIENTS
 		//ingredient 1
-		InsertIngredient(0,"SRP_MetalBucket_Default");//you can insert multiple ingredients this way
+		// InsertIngredient(0,"SRP_MetalBucket_Default");//you can insert multiple ingredients this way
+		InsertIngredient(0,"SRP_MetalBucket");//you can insert multiple ingredients this way
 		
 		m_IngredientAddHealth[0] = 0;// 0 = do nothing
 		m_IngredientSetHealth[0] = -1; // -1 = do nothing
