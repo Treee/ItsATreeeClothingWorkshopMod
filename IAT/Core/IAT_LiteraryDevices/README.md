6ffe052f8d2b2fcf65b74edf021e2d13c5cf6e6e
Treee <itsatreee.twitch@gmail.com>
2024-02-14 21:47:26 -0700

    adding hand held tablet

--- IAT/Core/IAT_LiteraryDevices/README.md
+++ IAT/Core/IAT_LiteraryDevices/README.md
@@ -7,3 +7,5 @@
 - "Old / Ancient Scroll" (https://skfb.ly/6SMq7) by Kigha is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
 - "Radio Tower Icon Vectors by Vecteezy" (https://www.vecteezy.com/free-vector/radio-tower-icon)
+
+- "Tablet PC - Forktab" (https://skfb.ly/6R6vr) by ForkyForklift is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
--- IAT/Core/IAT_LiteraryDevices/scripts/4_world/modded_itemregistered.c
+++ IAT/Core/IAT_LiteraryDevices/scripts/4_world/modded_itemregistered.c
@@ -5,5 +5,6 @@ modded class ModItemRegisterCallbacks
     super.RegisterOneHanded( pType, pBehavior );        
     // SCROLLS
     pType.AddItemInHandsProfileIK("IAT_LiteraryDevices_Scroll_Colorbase", "dz/anims/workspaces/player/player_main/weapons/player_main_1h_pipe.asi", pBehavior, "dz/anims/anm/player/ik/gear/hammer_ik.anm");    
+		pType.AddItemInHandsProfileIK("IAT_LiteraryDevices_HandHeldTablet_Colorbase", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/book.anm");
   };
 };
