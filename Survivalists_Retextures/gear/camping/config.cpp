36941c0b47813e30e6c3038a771301fd0e571e62
Treee <itsatreee.twitch@gmail.com>
2024-04-09 18:52:32 -0600

    adding flag retextures

--- Survivalists_Retextures/gear/camping/config.cpp
+++ Survivalists_Retextures/gear/camping/config.cpp
@@ -247,6 +247,637 @@ class CfgVehicles
 		color="WildTurkey";
 		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\flag_WildTurkey_co.paa"};
 	};  
+  
+  class Flag_4thOfJuly: Flag_Base
+	{
+		scope=2;
+		color="4thOfJuly";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_4thOfJuly_co.paa"};
+  };
+  class Flag_AlchemyWood: Flag_Base
+	{
+		scope=2;
+		color="AlchemyWood";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_AlchemyWood_co.paa"};
+  };
+  class Flag_AlchemyBlack: Flag_Base
+	{
+		scope=2;
+		color="AlchemyBlack";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_AlchemyBlack_co.paa"};
+  };
+  class Flag_Alien: Flag_Base
+	{
+		scope=2;
+		color="Alien";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Alien_co.paa"};
+  };
+  class Flag_AmericanIndian: Flag_Base
+	{
+		scope=2;
+		color="AmericanIndian";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_AmericanIndian_co.paa"};
+  };
+  class Flag_Bandit: Flag_Base
+	{
+		scope=2;
+		color="Bandit";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Bandit_co.paa"};
+  };
+  class Flag_BaphometDevil: Flag_Base
+	{
+		scope=2;
+		color="BaphometDevil";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_BaphometDevil_co.paa"};
+  };
+  class Flag_Barber: Flag_Base
+	{
+		scope=2;
+		color="Barber";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Barber_co.paa"};
+  };
+  class Flag_BeachSunset: Flag_Base
+	{
+		scope=2;
+		color="BeachSunset";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_BeachSunset_co.paa"};
+  };
+  class Flag_Biker: Flag_Base
+	{
+		scope=2;
+		color="Biker";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Biker_co.paa"};
+  };
+  class Flag_Blacksmith: Flag_Base
+	{
+		scope=2;
+		color="Blacksmith";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Blacksmith_co.paa"};
+  };
+  class Flag_Brewery: Flag_Base
+	{
+		scope=2;
+		color="Brewery";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Brewery_co.paa"};
+  };
+  class Flag_Builder: Flag_Base
+	{
+		scope=2;
+		color="Builder";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Builder_co.paa"};
+  };
+  class Flag_Butcher: Flag_Base
+	{
+		scope=2;
+		color="Butcher";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Butcher_co.paa"};
+  };
+  class Flag_Campfire: Flag_Base
+	{
+		scope=2;
+		color="Campfire";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Campfire_co.paa"};
+  };
+  class Flag_Camping: Flag_Base
+	{
+		scope=2;
+		color="Camping";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Camping_co.paa"};
+  };
+  class Flag_Carpentry: Flag_Base
+	{
+		scope=2;
+		color="Carpentry";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Carpentry_co.paa"};
+  };
+  class Flag_Circus: Flag_Base
+	{
+		scope=2;
+		color="Circus";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Circus_co.paa"};
+  };
+  class Flag_Club: Flag_Base
+	{
+		scope=2;
+		color="Club";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_CLub_co.paa"};
+  };
+  class Flag_ComedyClub: Flag_Base
+	{
+		scope=2;
+		color="ComedyClub";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_ComedyClub_co.paa"};
+  };
+  class Flag_CottageCoreFrog: Flag_Base
+	{
+		scope=2;
+		color="CottageCoreFrog";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_CottageCoreFrog_co.paa"};
+  };
+  class Flag_Courier: Flag_Base
+	{
+		scope=2;
+		color="Courier";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Courier_co.paa"};
+  };
+  class Flag_Cowboy: Flag_Base
+	{
+		scope=2;
+		color="Cowboy";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Cowboy_co.paa"};
+  };
+  class Flag_CrueltyFree: Flag_Base
+	{
+		scope=2;
+		color="CrueltyFree";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_CrueltyFree_co.paa"};
+  };
+  class Flag_Deli: Flag_Base
+	{
+		scope=2;
+		color="Deli";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Deli_co.paa"};
+  };
+  class Flag_Easter: Flag_Base
+	{
+		scope=2;
+		color="Easter";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Easter_co.paa"};
+  };
+  class Flag_Esoteric: Flag_Base
+	{
+		scope=2;
+		color="Esoteric";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Esoteric_co.paa"};
+  };
+  class Flag_EvilCat: Flag_Base
+	{
+		scope=2;
+		color="EvilCat";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_EvilCat_co.paa"};
+  };
+  class Flag_EyeofHorusEgyptian: Flag_Base
+	{
+		scope=2;
+		color="EyeofHorusEgyptian";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_EyeofHorusEgyptian_co.paa"};
+  };
+  class Flag_FaithHopeLove: Flag_Base
+	{
+		scope=2;
+		color="FaithHopeLove";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_FaithHopeLove_co.paa"};
+  };
+  class Flag_FarmersMarket: Flag_Base
+	{
+		scope=2;
+		color="FarmersMarket";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_FarmersMarket_co.paa"};
+  };
+  class Flag_Fisherman: Flag_Base
+	{
+		scope=2;
+		color="Fisherman";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Fisherman_co.paa"};
+  };
+  class Flag_FlowerSkull: Flag_Base
+	{
+		scope=2;
+		color="FlowerSkull";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_FlowerSkull_co.paa"};
+  };
+  class Flag_ForestStag: Flag_Base
+	{
+		scope=2;
+		color="ForestStag";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_ForestStag_co.paa"};
+  };
+  class Flag_FortuneTeller: Flag_Base
+	{
+		scope=2;
+		color="FortuneTeller";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_FortuneTeller_co.paa"};
+  };
+  class Flag_FuckOff: Flag_Base
+	{
+		scope=2;
+		color="FuckOff";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_FuckOff_co.paa"};
+  };
+  class Flag_Gambling: Flag_Base
+	{
+		scope=2;
+		color="Gambling";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Gambling_co.paa"};
+  };
+  class Flag_Ganesh: Flag_Base
+	{
+		scope=2;
+		color="Ganesh";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Ganesh_co.paa"};
+  };
+  class Flag_GothRose: Flag_Base
+	{
+		scope=2;
+		color="GothRose";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_GothRose_co.paa"};
+  };
+  class Flag_Graffiti: Flag_Base
+	{
+		scope=2;
+		color="Graffiti";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Graffiti_co.paa"};
+  };
+  class Flag_GunsandAmmo: Flag_Base
+	{
+		scope=2;
+		color="GunsandAmmo";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_GunsandAmmo_co.paa"};
+  };
+  class Flag_Halloween: Flag_Base
+	{
+		scope=2;
+		color="Halloween";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Halloween_co.paa"};
+  };
+  class Flag_HappyDiwali: Flag_Base
+	{
+		scope=2;
+		color="HappyDiwali";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_HappyDiwali_co.paa"};
+  };
+  class Flag_HappyPassover: Flag_Base
+	{
+		scope=2;
+		color="HappyPassover";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_HappyPassover_co.paa"};
+  };
+  class Flag_HippyPeace: Flag_Base
+	{
+		scope=2;
+		color="HippyPeace";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_HippyPeace_co.paa"};
+  };
+  class Flag_IceCream: Flag_Base
+	{
+		scope=2;
+		color="IceCream";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_IceCream_co.paa"};
+  };
+  class Flag_Jail: Flag_Base
+	{
+		scope=2;
+		color="Jail";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Jail_co.paa"};
+  };
+  class Flag_JapaneseteaHouse: Flag_Base
+	{
+		scope=2;
+		color="JapaneseteaHouse";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_JapaneseteaHouse_co.paa"};
+  };
+  class Flag_JapanRisingSun: Flag_Base
+	{
+		scope=2;
+		color="JapanRisingSun";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_JapanRisingSun_co.paa"};
+  };
+  class Flag_Jesus: Flag_Base
+	{
+		scope=2;
+		color="Jesus";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Jesus_co.paa"};
+  };
+  class Flag_Karaoke: Flag_Base
+	{
+		scope=2;
+		color="Karaoke";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Karaoke_co.paa"};
+  };
+  class Flag_KeepOut: Flag_Base
+	{
+		scope=2;
+		color="KeepOut";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_KeepOut_co.paa"};
+  };
+  class Flag_KillerClown: Flag_Base
+	{
+		scope=2;
+		color="KillerClown";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_KillerClown_co.paa"};
+  };
+  class Flag_Knight: Flag_Base
+	{
+		scope=2;
+		color="Knight";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Knight_co.paa"};
+  };
+  class Flag_LandoftheFree: Flag_Base
+	{
+		scope=2;
+		color="LandoftheFree";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_LandoftheFree_co.paa"};
+  };
+  class Flag_Lawyer: Flag_Base
+	{
+		scope=2;
+		color="Lawyer";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Lawyer_co.paa"};
+  };
+  class Flag_Leatherwork: Flag_Base
+	{
+		scope=2;
+		color="Leatherwork";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Leatherwork_co.paa"};
+  };
+  class Flag_Library: Flag_Base
+	{
+		scope=2;
+		color="Library";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Library_co.paa"};
+  };
+  class Flag_LiveMusic: Flag_Base
+	{
+		scope=2;
+		color="LiveMusic";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_LiveMusic_co.paa"};
+  };
+  class Flag_MedicalMarijuana: Flag_Base
+	{
+		scope=2;
+		color="MedicalMarijuana";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_MedicalMarijuana_co.paa"};
+  };
+  class Flag_Medical: Flag_Base
+	{
+		scope=2;
+		color="Medical";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Medical_co.paa"};
+  };
+  class Flag_Medusa: Flag_Base
+	{
+		scope=2;
+		color="Medusa";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Medusa_co.paa"};
+  };
+  class Flag_MerryXmas: Flag_Base
+	{
+		scope=2;
+		color="MerryXmas";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_MerryXmas_co.paa"};
+  };
+  class Flag_News: Flag_Base
+	{
+		scope=2;
+		color="News";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_News_co.paa"};
+  };
+  class Flag_NoFear: Flag_Base
+	{
+		scope=2;
+		color="NoFear";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_NoFear_co.paa"};
+  };
+  class Flag_Norse: Flag_Base
+	{
+		scope=2;
+		color="Norse";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Norse_co.paa"};
+  };
+  class Flag_OnAir: Flag_Base
+	{
+		scope=2;
+		color="OnAir";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_OnAir_co.paa"};
+  };
+  class Flag_Persian: Flag_Base
+	{
+		scope=2;
+		color="Persian";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Persian_co.paa"};
+  };
+  class Flag_Pizza: Flag_Base
+	{
+		scope=2;
+		color="Pizza";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Pizza_co.paa"};
+  };
+  class Flag_PrawnGod: Flag_Base
+	{
+		scope=2;
+		color="PrawnGod";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_PrawnGod_co.paa"};
+  };
+  class Flag_Quarantine: Flag_Base
+	{
+		scope=2;
+		color="Quarantine";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Quarantine_co.paa"};
+  };
+  class Flag_Radioactive: Flag_Base
+	{
+		scope=2;
+		color="Radioactive";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Radioactive_co.paa"};
+  };
+  class Flag_Ramadan: Flag_Base
+	{
+		scope=2;
+		color="Ramadan";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Ramadan_co.paa"};
+  };
+  class Flag_Raven: Flag_Base
+	{
+		scope=2;
+		color="Raven";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Raven_co.paa"};
+  };
+  class Flag_RecordStore: Flag_Base
+	{
+		scope=2;
+		color="RecordStore";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_RecordStore_co.paa"};
+  };
+  class Flag_SadPug: Flag_Base
+	{
+		scope=2;
+		color="SadPug";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_SadPug_co.paa"};
+  };
+  class Flag_Science: Flag_Base
+	{
+		scope=2;
+		color="Science";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Science_co.paa"};
+  };
+  class Flag_Scottish: Flag_Base
+	{
+		scope=2;
+		color="Scottish";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Scottish_co.paa"};
+  };
+  class Flag_ScubaClub: Flag_Base
+	{
+		scope=2;
+		color="ScubaClub";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_ScubaClub_co.paa"};
+  };
+  class Flag_Seafood: Flag_Base
+	{
+		scope=2;
+		color="Seafood";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Seafood_co.paa"};
+  };
+  class Flag_SkullandMoon: Flag_Base
+	{
+		scope=2;
+		color="SkullandMoon";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_SkullandMoon_co.paa"};
+  };
+  class Flag_Snowman: Flag_Base
+	{
+		scope=2;
+		color="Snowman";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Snowman_co.paa"};
+  };
+  class Flag_Stonemason: Flag_Base
+	{
+		scope=2;
+		color="Stonemason";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Stonemason_co.paa"};
+  };
+  class Flag_Stoner: Flag_Base
+	{
+		scope=2;
+		color="Stoner";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Stoner_co.paa"};
+  };
+  class Flag_StPatricksDay: Flag_Base
+	{
+		scope=2;
+		color="StPatricksDay";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_StPatricksDay_co.paa"};
+  };
+  class Flag_Taco: Flag_Base
+	{
+		scope=2;
+		color="Taco";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Taco_co.paa"};
+  };
+  class Flag_Tailor: Flag_Base
+	{
+		scope=2;
+		color="Tailor";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Tailor_co.paa"};
+  };
+  class Flag_Taxidermy: Flag_Base
+	{
+		scope=2;
+		color="Taxidermy";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Taxidermy_co.paa"};
+  };
+  class Flag_Thanksgiving: Flag_Base
+	{
+		scope=2;
+		color="Thanksgiving";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Thanksgiving_co.paa"};
+  };
+  class Flag_TheEndisNigh: Flag_Base
+	{
+		scope=2;
+		color="TheEndisNigh";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_TheEndisNigh_co.paa"};
+  };
+  class Flag_ThreeBears: Flag_Base
+	{
+		scope=2;
+		color="ThreeBears";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_ThreeBears_co.paa"};
+  };
+  class Flag_Tiger: Flag_Base
+	{
+		scope=2;
+		color="Tiger";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Tiger_co.paa"};
+  };
+  class Flag_Tinny: Flag_Base
+	{
+		scope=2;
+		color="Tinny";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Tinny_co.paa"};
+  };
+  class Flag_TownHall: Flag_Base
+	{
+		scope=2;
+		color="TownHall";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_TownHall_co.paa"};
+  };
+  class Flag_Trader: Flag_Base
+	{
+		scope=2;
+		color="Trader";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Trader_co.paa"};
+  };
+  class Flag_TrickorTreat: Flag_Base
+	{
+		scope=2;
+		color="TrickorTreat";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_TrickorTreat_co.paa"};
+  };
+  class Flag_Trustno1: Flag_Base
+	{
+		scope=2;
+		color="Trustno1";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Trustno1_co.paa"};
+  };
+  class Flag_UnicornVomit: Flag_Base
+	{
+		scope=2;
+		color="UnicornVomit";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_UnicornVomit_co.paa"};
+  };
+  class Flag_University: Flag_Base
+	{
+		scope=2;
+		color="University";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_University_co.paa"};
+  };
+  class Flag_ValentineDay: Flag_Base
+	{
+		scope=2;
+		color="ValentineDay";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_ValentineDay_co.paa"};
+  };
+  class Flag_VampyricCastle: Flag_Base
+	{
+		scope=2;
+		color="VampyricCastle";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_VampyricCastle_co.paa"};
+  };
+  class Flag_Vet: Flag_Base
+	{
+		scope=2;
+		color="Vet";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Vet_co.paa"};
+  };
+  class Flag_Wicca: Flag_Base
+	{
+		scope=2;
+		color="Wicca";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Wicca_co.paa"};
+  };
+  class Flag_Wizard: Flag_Base
+	{
+		scope=2;
+		color="Wizard";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Wizard_co.paa"};
+  };
+  class Flag_Yeti: Flag_Base
+	{
+		scope=2;
+		color="Yeti";
+		hiddenSelectionsTextures[]={"Survivalists_Retextures\gear\camping\data\Flag_Yeti_co.paa"};
+  };
 //================================================ MODDED  
   class SRP_Guitar_Alpocalypse_Blue: SRP_Guitar_Alpocalypse
 	{
