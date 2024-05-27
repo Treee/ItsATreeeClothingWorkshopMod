6659e3170d591dd9d141970a36782f5f0166a0f5
Treee <itsatreee.twitch@gmail.com>
2024-05-27 12:51:02 -0600

    fixing gasoline evaporation when transfering to jerry can

--- Survivalists_Scripts/scripts/4_world/entities/itembase/containers/modded_bottlebase.c
+++ Survivalists_Scripts/scripts/4_world/entities/itembase/containers/modded_bottlebase.c
@@ -1,17 +1,19 @@
 modded class Bottle_Base
 {
+    protected bool m_HasEvaporated = false;
 	override void AfterStoreLoad()
-	{	
+	{
 		super.AfterStoreLoad();
-		
-    if (GetGame().IsDedicatedServer())
-    {
-      if (GetLiquidType() == LIQUID_GASOLINE)
-      {
-        float randomEvaporation = Math.RandomFloatInclusive(0.01, 0.07);
-        int newQuantity = GetQuantity() * randomEvaporation; // 10% for jesus
-        AddQuantity(-newQuantity);
-      }        
-    }
+
+        if (GetGame().IsDedicatedServer())
+        {
+            if (!m_HasEvaporated && GetLiquidType() == LIQUID_GASOLINE)
+            {
+                float randomEvaporation = Math.RandomFloatInclusive(0.01, 0.07);
+                int newQuantity = GetQuantity() * randomEvaporation; // 10% for jesus
+                AddQuantity(-newQuantity);
+                m_HasEvaporated = true;
+            }
+        }
 	}
 };
