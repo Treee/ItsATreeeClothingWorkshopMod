f0587aacdbf05447bc5b01ae991896857901b83a
Treee <itsatreee.twitch@gmail.com>
2024-06-13 21:21:22 -0600

    adding piglet cookies to radioactive box protection

--- Survivalists_Scripts/scripts/4_world/entities/itembase/containers/srp_protectedcase.c
+++ Survivalists_Scripts/scripts/4_world/entities/itembase/containers/srp_protectedcase.c
@@ -1,54 +1,62 @@
 class SRP_SmallProtectorCase_Nails extends SmallProtectorCase
 {
-  override bool CanReceiveItemIntoCargo (EntityAI item)
+    override bool CanReceiveItemIntoCargo (EntityAI item)
 	{
 		return item.IsKindOf("Nail") || item.IsKindOf("NailBox");
 	}
 
-  override bool CanLoadItemIntoCargo( EntityAI item )
-  {
-		return item.IsKindOf("Nail") || item.IsKindOf("NailBox");
-  }
+    override bool CanLoadItemIntoCargo( EntityAI item )
+    {
+        return item.IsKindOf("Nail") || item.IsKindOf("NailBox");
+    }
 };
 class SRP_SmallProtectorCase_Syringe extends SmallProtectorCase
 {
-  override bool CanReceiveItemIntoCargo (EntityAI item)
-	{
-    if (item.GetType().Contains("DUB_Mutant"))
-      return true;
-    if (item.IsInherited(AntiChemInjector))
-      return true;
-    if (item.IsInherited(SRP_FullSyringe_Base))
-      return true;
-    if (item.IsInherited(SRP_InjectionVial_Base))
-      return true;
-    if (item.IsInherited(BloodSyringe))
-      return true;
-    if (item.IsInherited(Syringe))
-      return true;
-    if (item.IsInherited(ClearSyringe))
-      return true;
-    return false;    
-	}
+    override bool CanReceiveItemIntoCargo (EntityAI item)
+    {
+        if (item.GetType().Contains("DUB_Mutant"))
+            return true;
+        if (item.IsInherited(AntiChemInjector))
+            return true;
+        if (item.IsInherited(SRP_FullSyringe_Base))
+            return true;
+        if (item.IsInherited(SRP_InjectionVial_Base))
+            return true;
+        if (item.IsInherited(BloodSyringe))
+            return true;
+        if (item.IsInherited(Syringe))
+            return true;
+        if (item.IsInherited(ClearSyringe))
+            return true;
+        if (item.IsInherited(Honey_Gluttony))
+            return true;
+        if (item.IsInherited(SRP_Guts_PigletCookiesAcid))
+            return true;
+        return false;
+    }
 
-  override bool CanLoadItemIntoCargo( EntityAI item )
-  {
-    if (item.GetType().Contains("DUB_Mutant"))
-      return true;
-    if (item.IsInherited(AntiChemInjector))
-      return true;
-    if (item.IsInherited(SRP_FullSyringe_Base))
-      return true;
-    if (item.IsInherited(SRP_InjectionVial_Base))
-      return true;
-    if (item.IsInherited(BloodSyringe))
-      return true;
-    if (item.IsInherited(Syringe))
-      return true;
-    if (item.IsInherited(ClearSyringe))
-      return true;
-    return false;     
-  }
+    override bool CanLoadItemIntoCargo( EntityAI item )
+    {
+        if (item.GetType().Contains("DUB_Mutant"))
+            return true;
+        if (item.IsInherited(AntiChemInjector))
+            return true;
+        if (item.IsInherited(SRP_FullSyringe_Base))
+            return true;
+        if (item.IsInherited(SRP_InjectionVial_Base))
+            return true;
+        if (item.IsInherited(BloodSyringe))
+            return true;
+        if (item.IsInherited(Syringe))
+            return true;
+        if (item.IsInherited(ClearSyringe))
+            return true;
+        if (item.IsInherited(Honey_Gluttony))
+            return true;
+        if (item.IsInherited(SRP_Guts_PigletCookiesAcid))
+            return true;
+        return false;
+    }
 };
 class SRP_SmallProtectorCase_FlareBox extends SmallProtectorCase
 {
@@ -75,7 +83,7 @@ class SRP_SmallProtectorCase_FlagArmbandBox extends SmallProtectorCase
     if (item.IsInherited(SRP_PatchLogo_ColorBase))
       return true;
     if (item.IsInherited(Flag_Base))
-      return true;    
+      return true;
     return false;
 	}
 
@@ -90,7 +98,7 @@ class SRP_SmallProtectorCase_FlagArmbandBox extends SmallProtectorCase
     if (item.IsInherited(SRP_PatchLogo_ColorBase))
       return true;
     if (item.IsInherited(Flag_Base))
-      return true;    
+      return true;
     return false;
   }
 };
