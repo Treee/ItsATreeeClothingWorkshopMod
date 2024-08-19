f1a1c809613cb60f78cb27a7d4d4d81a0e2f17fd
Treee <itsatreee.twitch@gmail.com>
2024-08-19 16:21:44 -0600

    replacing default texture with investor path

--- Survivalists_Characters/masks/config.cpp
+++ Survivalists_Characters/masks/config.cpp
@@ -13,7 +13,8 @@ class CfgPatches
             "Scarf",
             "Dressupbox",
             "Alv_Tac_Gear_Masks",
-            "Survivalists_Narcotics_Gear_Clothing"
+            "Survivalists_Narcotics_Gear_Clothing",
+            "SRP_Core_Characters_Masks"
         };
     };
 };
@@ -21,219 +22,8 @@ class CfgVehicles
 {
     // class Mask_Base;
     class Clothing;
-    //------------------------------------- BASE GAME OVERRIDE
-    class GasMask : Clothing
-    {
-        soundVoiceType = "none";
-        soundVoicePriority = 3;
-        itemSize[] = { 2,2 };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class BalaclavaMask_ColorBase : Clothing
-    {
-        itemSize[] = { 1,2 };
-        rootClassName = "BalaclavaMask";
-        colorVariants[] =
-        {
-          "Black",
-          "BlackSkull",
-          "Beige",
-          "Blue",
-          "Green",
-          "Pink",
-          "White",
-          "MTP",
-          "ATAC",
-          "ATACSFG",
-          "USNAOR1",
-          "USNAOR2",
-          "SURPAT",
-          "MARPATWinter",
-          "EMP6",
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class Balaclava3Holes_ColorBase : Clothing
-    {
-        itemSize[] = { 1,2 };
-        rootClassName = "Balaclava3Holes";
-        colorVariants[] =
-        {
-          "Black",
-          "BlackSkull",
-          "Beige",
-          "Blue",
-          "Green",
-          "Pink",
-          "White",
-          "MTP",
-          "ATAC",
-          "ATACSFG",
-          "USNAOR1",
-          "USNAOR2",
-          "SURPAT",
-          "MARPATWinter",
-          "EMP6",
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class GP5GasMask : Clothing
-    {
-        soundVoiceType = "none";
-        soundVoicePriority = 3;
-        itemSize[] = { 2,2 };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class SurgicalMask : Clothing
-    {
-        rootClassName = "SurgicalMask";
-        colorVariants[] =
-        {
-          "Black"
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class HockeyMask : Clothing
-    {
-        rootClassName = "HockeyMask";
-        colorVariants[] =
-        {
-          "Steel"
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class MimeMask_Male : Clothing
-    {
-        rootClassName = "MimeMask_Male";
-        colorVariants[] =
-        {
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class MimeMask_Female : MimeMask_Male
-    {
-        rootClassName = "MimeMask_Female";
-        colorVariants[] =
-        {
-        };
-        inventorySlot[] +=
-        {
-            "SRP_Mask1",
-                "SRP_Mask2",
-                "SRP_Mask3",
-                "SRP_Mask4",
-                "SRP_Mask5",
-                "SRP_Mask6",
-                "SRP_Mask7",
-                "SRP_Mask8",
-                "SRP_Mask9",
-                "SRP_Mask10",
-        };
-    };
-    class FaceCover_Improvised : Clothing
-    {
-        rootClassName = "FaceCover_Improvised";
-        colorVariants[] =
-        {
-          "Green",
-          "Black",
-          "Orange",
-          "Blue",
-          "Red",
-        };
-    };
-    class NioshFaceMask : Clothing
-    {
-        rootClassName = "NioshFaceMask";
-        colorVariants[] =
-        {
-          "WaldoRescue",
-        };
-    };
+    class GP5GasMask;
+
     //------------------------------------- ALEVARIC OVERRIDE
     class ALV_MV_MouthCover_Colorbase : Clothing
     {
@@ -2727,7 +2517,7 @@ class CfgVehicles
         };
         hiddenSelectionsTextures[] =
         {
-            "Survivalists_Characters\masks\data\srp_mustache_co.paa"
+            "Survivalists_Characters\masks\data\srp_mustache_baaklavah_co.paa"
         };
     };
 
