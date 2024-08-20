9d2a731b8825eec032e3272f9e94d66f9dd287bb
Treee <itsatreee.twitch@gmail.com>
2024-08-20 12:37:16 -0600

    removing stone tax on flags

--- Survivalists_Scripts/scripts/4_world/actions/continuous/modded_actionraiseflag.c
+++ Survivalists_Scripts/scripts/4_world/actions/continuous/modded_actionraiseflag.c
@@ -1,41 +1,53 @@
-modded class ActionRaiseFlag
-{	
-  override void OnActionInfoUpdate( PlayerBase player, ActionTarget target, ItemBase item )
-	{
-    m_Text = "#raise_flag";
+// modded class ActionRaiseFlag
+// {
+//     override void OnActionInfoUpdate( PlayerBase player, ActionTarget target, ItemBase item )
+//     {
+//         m_Text = "#raise_flag";
 
-    TerritoryFlag totem;
-    if (Class.CastTo(totem, target.GetObject()))
-    {
-      ItemBase stoneTax;
-      if (!Class.CastTo(stoneTax, totem.FindAttachmentBySlotName("StonesFlagTax")))
-      {
-        m_Text = "Raise Flag - Requires Large Stones";
-      }
-    }
-	}
-  override void OnFinishProgressServer( ActionData action_data )
-	{
-    TerritoryFlag totem;
-    if (!Class.CastTo(totem, action_data.m_Target.GetObject()))
-      return;
-    
-    ItemBase stoneTax;
-    if (!Class.CastTo(stoneTax, totem.FindAttachmentBySlotName("StonesFlagTax")))        
-      return;      
-      
-    stoneTax.AddQuantity(-1);      
-    
-    super.OnFinishProgressServer(action_data);
-  }
-};
+//         TerritoryFlag totem;
+//         if (Class.CastTo(totem, target.GetObject()))
+//         {
+//             if (!totem.HasStonesForTax())
+//                 m_Text = "Raise Flag - Requires Large Stones";
+//         }
+//     }
+//     override void OnFinishProgressServer( ActionData action_data )
+//     {
+//         TerritoryFlag totem;
+//         if (!Class.CastTo(totem, action_data.m_Target.GetObject()))
+//             return;
 
-modded class TerritoryFlag
-{
-  override bool CanReceiveAttachment( EntityAI attachment, int slotId )
-  {
-    if (attachment.GetType() == "Stone")
-      return true;
-    return super.CanReceiveAttachment(attachment, slotId);
-  }
-};
+//         if (!totem.HasStonesForTax())
+//             return;
+
+//         totem.RemoveStoneForTax();
+//         super.OnFinishProgressServer(action_data);
+//     }
+// };
+
+// modded class TerritoryFlag
+// {
+//     override bool CanReceiveAttachment( EntityAI attachment, int slotId )
+//     {
+//         if (attachment.GetType() == "Stone")
+//             return true;
+//         return super.CanReceiveAttachment(attachment, slotId);
+//     }
+
+//     bool HasStonesForTax()
+//     {
+//         ItemBase stoneTax;
+//         if (Class.CastTo(stoneTax, FindAttachmentBySlotName("StonesFlagTax")))
+//         {
+//             return stoneTax.GetQuantity() > 1;
+//         }
+//         return false;
+//     }
+
+//     void RemoveStoneForTax()
+//     {
+//         ItemBase stoneTax;
+//         if (Class.CastTo(stoneTax, FindAttachmentBySlotName("StonesFlagTax")))
+//             stoneTax.AddQuantity(-1);
+//     }
+// };
